de67b5d6b3dbd1e453880a787c1a537ea48046bf
jward3 <jward3@email.unc.edu>
2019-07-12 11:15:53 -0400

    Fix skipTrigger when used with bootstrap button groups

--- resources/views/layouts/survey.blade.php
+++ resources/views/layouts/survey.blade.php
@@ -9,8 +9,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -30,6 +28,7 @@
         </main>
     </div>
 
+    <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}"></script>
     @stack('scripts')
 </body>
