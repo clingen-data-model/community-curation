becd0c83ae07dd39b969023613678ef61f2b2247
jward3 <jward3@email.unc.edu>
2019-07-15 11:43:13 -0400

    Add ExpertPanelsTableSeeder to DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesAndPermissionTablesSeeder::class);
         $this->call(CampaignsTableSeeder::class);
         $this->call(CurationActivitiesTableSeeder::class);
+        $this->call(ExpertPanelsTableSeeder::class);
         $this->call(GoalsTableSeeder::class);
         $this->call(InterestsTableSeeder::class);
         $this->call(MotivationsTableSeeder::class);
