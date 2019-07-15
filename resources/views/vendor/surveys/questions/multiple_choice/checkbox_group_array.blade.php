ed9b950877417630540db0071e608fc5fe0151fe
jward3 <jward3@email.unc.edu>
2019-07-15 11:44:31 -0400

    Clean up styling for application survey

---
+++ resources/views/vendor/surveys/questions/multiple_choice/checkbox_group_array.blade.php
@@ -0,0 +1,45 @@
+@component('surveys::questions.question', compact('renderable', 'context'))
+
+  @slot('answers')
+    <div class="btn-group-vertical" role="group">
+      @foreach($renderable->options as $idx => $option)
+        <label class="checkbox {{$option->class}}">
+        <input type="checkbox" 
+          name="{{ $renderable->name }}[{{$option->value}}]"
+          id="{{ $renderable->name }}-{{$idx}}_checkbox" 
+          class="{{ $option->class}}"
+          autocomplete="off"
+          value="1"
+          @if( isset($context['response']->{$renderable->name}) 
+              && isset($context['response']->{$renderable->name}[$option->value]) 
+              && $context['response']->{$renderable->name}[$option->value] == 1
+          )
+            checked="checked"
+          @endif
+          @if($option->show)
+            data-skipTarget="{{$option->show}}"
+          @endif
+          @if($option->hide)
+            data-hide="{{$option->hide}}"
+          @endif
+          @if($option->class)
+            class="{{$option->class}}"
+          @endif
+          />
+          {!! $option->getCompiledLabel($context) !!}
+      </label>
+      @endforeach
+    </div>
+  @endslot
+
+  @slot('errors-block')
+    @if ( isset($context['errors']) 
+          && count(array_intersect( array_keys($context['errors']->getMessages()), $renderable->getOptionNames() )) > 0 )
+      <div class="error-block">
+        <ul class="error-list list-unstyled">
+          <li>A response is required for this question</li>
+        </ul>
+      </div>
+    @endif
+  @endslot
+@endcomponent
