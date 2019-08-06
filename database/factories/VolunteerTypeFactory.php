64cb87499e11e5d84cee372bfff4a68fc1712345
jward3 <jward3@email.unc.edu>
2019-08-06 10:57:29 -0400

    Add VolunteerTypes

---
+++ database/factories/VolunteerTypeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\VolunteerType;
+use Faker\Generator as Faker;
+
+$factory->define(VolunteerType::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word
+    ];
+});
---
+++ database/migrations/2019_08_06_144954_create_volunteer_types_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVolunteerTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('volunteer_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('volunteer_types');
+    }
+}
