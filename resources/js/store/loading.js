054b8e6e327258c7d0c5f93c6b0b6b11c277bc59
jward3 <jward3@email.unc.edu>
2019-08-08 15:22:55 -0400

    Setting up for building out ui

---
+++ resources/js/store/loading.js
@@ -0,0 +1,53 @@
+const state = {
+    requestCount: 0,
+    apiRequestCounts: {
+        omim: 0,
+        mondo: 0,
+        pubmed: 0,
+    }
+}
+
+const getters = {
+    loading(state) {
+        return state.requestCount > 0
+    },
+    apiLoading(state, apiKey) {
+        if (typeof apiKey == 'object') {
+            return false;
+        }
+        if (Object.keys(state.apiRequestCounts).indexOf(apiKey) < 0) {
+            throw new Error(apiKey + ' is not a valid key for apiRequestCounts.')
+        }
+        return state.apiRequestCounts[apiKey] > 0
+    },
+    omimLoading(state) {
+        return state.apiRequestCounts['omim'] > 0
+    }
+}
+
+const mutations = {
+    addRequest(state) {
+        state.requestCount++;
+    },
+    removeRequest(state) {
+        state.requestCount--;
+    },
+    addApiRequest(state, apiKey) {
+        if (typeof apiKey == 'object') {
+            return false;
+        }
+        if (Object.keys(state.apiRequestCounts).indexOf(apiKey) < 0) {
+            throw new Error(apiKey + ' is not a valid key for apiRequestCounts.')
+        }
+        state.apiRequestCounts[apiKey]++
+    },
+    removeApiRequest(state, apiKey) {
+        if (typeof apiKey == 'object') {
+            return false;
+        }
+        if (Object.keys(state.apiRequestCounts).indexOf(apiKey) < 0) {
+            throw new Error(apiKey + ' is not a valid key for apiRequestCounts.')
+        }
+        state.apiRequestCounts[apiKey]--
+    }
+}
