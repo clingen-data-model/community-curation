7285bf73ae32199f761cc6b418ed34bf3a719773
jward3 <jward3@email.unc.edu>
2019-08-08 10:47:25 -0400

    Add missing VolunteerType request and policy

---
+++ app/Policies/VolunteerTypePolicy.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\VolunteerType;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class VolunteerStatusType
+{
+    use HandlesAuthorization;
+    
+    /**
+     * Determine whether the user can view any volunteer statuses.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        $user->hasPermissionTo('list volunteer-types');
+    }
+
+    /**
+     * Determine whether the user can view the volunteer status.
+     *
+     * @param  \App\User  $user
+     * @param  \App\VolunteerType  $volunteerType
+     * @return mixed
+     */
+    public function view(User $user, VolunteerType $volunteerType)
+    {
+        $user->hasPermissionTo('list volunteer-types');
+    }
+
+    /**
+     * Determine whether the user can create volunteer statuses.
+     *
+     * @param  \App\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        $user->hasPermissionTo('create volunteer-types');
+    }
+
+    /**
+     * Determine whether the user can update the volunteer status.
+     *
+     * @param  \App\User  $user
+     * @param  \App\VolunteerType  $volunteerType
+     * @return mixed
+     */
+    public function update(User $user, VolunteerType $volunteerType)
+    {
+        $user->hasPermissionTo('update volunteer-types');
+    }
+
+    /**
+     * Determine whether the user can delete the volunteer status.
+     *
+     * @param  \App\User  $user
+     * @param  \App\VolunteerType  $volunteerType
+     * @return mixed
+     */
+    public function delete(User $user, VolunteerType $volunteerType)
+    {
+        $user->hasPermissionTo('delete volunteer-types');
+    }
+
+    /**
+     * Determine whether the user can restore the volunteer status.
+     *
+     * @param  \App\User  $user
+     * @param  \App\VolunteerType  $volunteerType
+     * @return mixed
+     */
+    public function restore(User $user, VolunteerType $volunteerType)
+    {
+        $user->hasPermissionTo('delete volunteer-types');
+    }
+
+    /**
+     * Determine whether the user can permanently delete the volunteer status.
+     *
+     * @param  \App\User  $user
+     * @param  \App\VolunteerType  $volunteerType
+     * @return mixed
+     */
+    public function forceDelete(User $user, VolunteerType $volunteerType)
+    {
+        $user->hasPermissionTo('delete volunteer-types');
+    }
+}
