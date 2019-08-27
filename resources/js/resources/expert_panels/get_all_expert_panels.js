43b93f61804f788f8ccfa711d72ac1a642d68797
jward3 <jward3@email.unc.edu>
2019-08-27 14:23:35 -0400

    Remove caching of expert panels to ensure always up to date

--- resources/js/resources/expert_panels/get_all_expert_panels.js
+++ resources/js/resources/expert_panels/get_all_expert_panels.js
@@ -1,12 +1,13 @@
 const getAllCurationActivities = async function () {
-    let data = JSON.parse(localStorage.getItem('expert-panels'));
-
-    if (!data) {
+    // let cacheLifeTime = 60*10*1000;
+    // let data = JSON.parse(localStorage.getItem('expert-panels'));
+    let data = [];
+    // if (!data) {
         window.axios.get('/api/expert-panels').then(response => {
             data = response.data.data
-            localStorage.setItem('expert-panels', JSON.stringify(data))
+            // localStorage.setItem('expert-panels', JSON.stringify(data))
         });
-    }
+    // }
 
     return data;
 }
