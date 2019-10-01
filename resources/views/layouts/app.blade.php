e3a8e5a61dfb60bd47c8cc99a09ca1f8461ff037
jward3 <jward3@email.unc.edu>
2019-10-01 16:54:26 -0400

    call clearSessionStorage when logging out or impersonating

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,9 @@
 
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                     document.getElementById('logout-form').submit();"
+                                        @click="clearSessionStorage"
+                                    >
                                         {{ __('Logout') }}
                                     </a>
 
