80f9bdf7b7e6b81a41f71d409f013ede30db66f1
jward3 <jward3@email.unc.edu>
2019-10-01 16:25:31 -0400

    Remove console.log from get_all_curation_activities.js

--- resources/js/resources/curation_activities/get_all_curation_activities.js
+++ resources/js/resources/curation_activities/get_all_curation_activities.js
@@ -3,7 +3,6 @@ const getAllCurationActivities = async function () {
 
     if (!data) {
         window.axios.get('/api/curation-activities').then(response => {
-            console.log('requesting curation activities');
             data  = response.data.data
             localStorage.setItem('curation-activities', JSON.stringify(data))
         });
