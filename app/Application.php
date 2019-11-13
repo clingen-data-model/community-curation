fa6f9d4093c4b9155f50de1a0e6ae61ffb4ade10
jward3 <jward3@email.unc.edu>
2019-11-13 15:38:58 -0500

    Add relations and accessors to Application model

--- app/Application.php
+++ app/Application.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Campaign;
 use Illuminate\Database\Eloquent\Model;
 
 class Application extends Model
@@ -25,4 +26,72 @@ class Application extends Model
     {
         return $this->respondent();
     }
+
+    public function survey()
+    {
+        return $this->belongsTo(class_survey());
+    }
+    
+
+    public function country()
+    {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function volunteerType()
+    {
+        return $this->belongsTo(VolunteerType::class, 'volunteer_type');
+    }
+
+    public function selfDescription()
+    {
+        return $this->belongsTo(SelfDescription::class, 'self_desc');
+    }
+
+
+    public function getSurveyDocAttribute()
+    {
+        return $this->survey->document;
+    }
+
+    public function getHighestEdAttribute()
+    {
+        return $this->getValueFromOptions('highest_ed');
+    }
+
+    public function getTimezoneAttribute()
+    {
+        return $this->getValueFromOptions('timezone');
+    }
+
+    public function getAdditionalPriorityAttribute()
+    {
+        return $this->getValueFromOptions('additional_priority');
+    }
+
+    public function getAdCampaignAttribute()
+    {
+        return Campaign::find(json_decode($this->attributes['ad_campaign']))->pluck('name');
+    }
+
+    public function getMotivationAttribute()
+    {
+        return Motivation::find(json_decode($this->attributes['motivation']))->pluck('name');
+    }
+
+    public function getGoalsAttribute()
+    {
+        return Goal::find(json_decode($this->attributes['goals']))->pluck('name');
+    }
+
+    public function getInterestsAttribute()
+    {
+        return Interest::find(json_decode($this->attributes['interests']))->pluck('name');
+    }
+
+    private function getValueFromOptions($attribute)
+    {
+        return $this->survey->document->questions[$attribute]->getOptionByValue($this->attributes[$attribute])->label;
+    }
+    
 }
