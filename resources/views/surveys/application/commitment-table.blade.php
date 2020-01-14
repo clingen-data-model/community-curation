4eb7a41748927c2927d6dcda0a8784ee39758037
jward3 <jward3@email.unc.edu>
2020-01-14 15:57:56 -0500

    Updates to application basedon client feedback

--- resources/views/surveys/application/commitment-table.blade.php
+++ resources/views/surveys/application/commitment-table.blade.php
@@ -2,9 +2,9 @@
 <table class="table table-striped text-center">
     <thead>
         <tr>
-            <th style="width: 40%">&nbsp;</th>
-            <th class="text-center" style="width: 30%">Comprehensive</th>
-            <th class="text-center" style="width: 30%">Baseline</th>
+            <th style="width: 30%">&nbsp;</th>
+            <th class="text-center" style="width: 35%">Comprehensive</th>
+            <th class="text-center" style="width: 35%">Baseline</th>
         </tr>
     </thead>
     <tbody>
@@ -19,7 +19,7 @@
             <td style="font-size: 1.5rem;">✅</td>
         </tr>
         <tr>
-            <td class="text-left">Participate on conference calls with experts</td>
+            <td class="text-left">Join and participate on expert panel conference calls.</td>
             <td style="font-size: 1.5rem;">✅</td>
             <td>&nbsp;</td>
         </tr>
@@ -33,7 +33,7 @@
         </tr>
         <tr>
             <td class="text-left">Time Commitment</td>
-            <td><small>Approximately 8-10 hours/month</small></td>
+            <td><small>~8-10 hours/ month, for a minimum of 6 months</small></td>
             <td>
                 <small>
                     No commitment
@@ -49,7 +49,7 @@
         <tr data-toggle="buttons">
             <td>&nbsp;</td>
             <td class="btn-group-toggle">
-                <label class="btn btn-lg btn-light @if ($context['response']->{$renderable->name} == 2) active @endif">
+                <label class="commitment-button btn btn-lg btn-light form-control @if ($context['response']->{$renderable->name} == 2) active @endif">
                     <input 
                         type="radio" 
                         name="{{$renderable->name}}" 
@@ -64,7 +64,7 @@
                 </label>
             </td>
             <td class="btn-group-toggle">
-                    <label class="btn btn-lg btn-light @if ($context['response']->{$renderable->name} == 1) active @endif">
+                    <label class="commitment-button btn btn-lg btn-light form-control @if ($context['response']->{$renderable->name} == 1) active @endif">
                         <input 
                             type="radio" 
                             name="{{$renderable->name}}" 
