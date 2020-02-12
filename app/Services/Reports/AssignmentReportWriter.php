6b29b3a26bac48129b6660eacc0e6f48c5c33c1d
TJ Ward <jward3@email.unc.edu>
2020-02-12 13:14:29 -0500

    Add route/endpoint to get xlsx output of AssignmentsReport

---
+++ app/Services/Reports/AssignmentReportWriter.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Services\Reports;
+
+use Carbon\Carbon;
+use Box\Spout\Writer\XLSX\Writer as XlsxWriter;
+use Box\Spout\Writer\Common\Creator\Style\StyleBuilder;
+use Box\Spout\Writer\Common\Creator\WriterEntityFactory;
+
+class AssignmentReportWriter
+{
+    protected $xlsxWriter;
+
+    public function __construct(XlsxWriter $writer)
+    {
+        $this->xlsxWriter = $writer;
+    }
+
+    public function setPath($path):AssignmentReportWriter
+    {
+        $this->xlsxWriter->openToFile($path);
+        return $this;
+    }
+
+    public function writeData($data)
+    {
+        $headerStyle = $rowStyle = (new StyleBuilder())
+            ->setFontBold()
+            ->build();
+
+        foreach ($data as $sheetName => $data) {
+            $sheet = $this->xlsxWriter->getCurrentSheet();
+            $sheet->setName($sheetName);
+
+            $this->xlsxWriter->addRow(WriterEntityFactory::createRow($this->getHeaderCells($data), $rowStyle));
+
+            foreach ($data->toArray() as $rowData) {
+                $row = WriterEntityFactory::createRow($this->arrayToCells($rowData));
+                $this->xlsxWriter->addRow($row);
+            }
+
+            $this->xlsxWriter->addNewSheetAndMakeItCurrent();
+        }
+
+        $this->xlsxWriter->close();        
+    }
+    private function arrayToCells(array $array) 
+    {
+        return array_map(function ($item) {
+            $value = $item;
+            if (is_object($item)) {
+                $value = $item->toString();
+                if (get_class($item) == Carbon::class) {
+                    $value = $item->format("Y-m-d H:i:s");
+                }
+            }
+            return WriterEntityFactory::createCell($value);
+        }, $array);
+
+    }
+
+    private function getHeaderCells($data)
+    {
+        return collect($data->first())->keys()
+            ->transform(function ($heading) {
+                return WriterEntityFactory::createCell(preg_replace('/_/', ' ', title_case($heading)));
+            })
+            ->toArray();
+    }
+    
+}
