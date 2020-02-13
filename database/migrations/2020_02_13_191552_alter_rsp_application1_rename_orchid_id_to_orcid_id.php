37a654433c4ecca7c148ff6e7fdf6d341f7e645a
TJ Ward <jward3@email.unc.edu>
2020-02-13 14:25:09 -0500

    Fix ORCID spelling

---
+++ database/migrations/2020_02_13_191552_alter_rsp_application1_rename_orchid_id_to_orcid_id.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterRspApplication1RenameOrchidIdToOrcidId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rsp_application_1', function (Blueprint $table) {
+            $table->renameColumn('orchid_id', 'orcid_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rsp_application_1', function (Blueprint $table) {
+            $table->renameColumn('orcid_id', 'orchid_id');
+        });
+    }
+}
