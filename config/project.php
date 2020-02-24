27cbed3ac185052ba2f05d481751542e78ab2882
TJ Ward <jward3@email.unc.edu>
2020-02-24 13:42:30 -0500

    Reorganize volunteer configs into volunteer

--- config/project.php
+++ config/project.php
@@ -8,18 +8,6 @@ return [
         CurationActivity::class,
         ExpertPanel::class
     ],
-    'volunteer-statuses' => [
-        'applied' => 1,
-        'trained' => 2,
-        'active' => 3,
-        'unresponsive' => 4,
-        'declined' => 5,
-        'retired' => 6,
-    ],
-    'volunteer_types' => [
-        'baseline' => 1,
-        'comprehensive' => 2
-    ],
     'assignment-statuses' => [
         'assigned' => 1,
         'trained' => 2,
