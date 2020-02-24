4692c54e09796092cc354df6eb62ebde771d42c9
TJ Ward <jward3@email.unc.edu>
2020-02-24 13:42:51 -0500

    Assign Baseline CA when user given type Baseline

---
+++ tests/Feature/BaselineVolunteerAssignedToBaselineTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\CurationActivity;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+/**
+ * @group baseline
+ */
+class BaselineVolunteerAssignedToBaselineTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setup():void
+    {
+        parent::setup();
+        $this->volunteer = factory(User::class)->state('baseline')->create([]);
+    }
+
+    /**
+     * @test
+     */
+    public function volunteer_assigned_baseline_ca_when_given_type_baseline()
+    {
+        $this->assertNotNull($this->volunteer->assignments()->first());
+        $this->assertEquals(CurationActivity::findByName('baseline'), $this->volunteer->assignments()->first()->assignable);
+    }
+}
