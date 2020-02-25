be9a0e7e58702cf7dacf7d8a435f48de117b9630
TJ Ward <jward3@email.unc.edu>
2020-02-25 17:13:52 -0500

    Add a command to send three month notifications to volunteers.

---
+++ tests/Feature/VolunteerThreeMonthFollowupNotificationTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Carbon\Carbon;
+use Tests\TestCase;
+use App\CurationActivity;
+use Illuminate\Support\Facades\Mail;
+use App\Jobs\AssignVolunteerToAssignable;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Notification;
+use App\Notifications\ThreeMonthVolunteerFollowup;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * @group comprehensive
+ * @group 90-day-followup
+ */
+class VolunteerThreeMonthFollowupNotificationTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setup():void
+    {
+        parent::setup();
+    }
+
+    /**
+     * @test
+     */
+    public function comprehensive_volunteers_emailed_90_days_after_ep_assignment()
+    {
+        Notification::fake();
+        $volunteer = factory(User::class)->state('comprehensive')->create([]);
+        $volunteer->assignRole('volunteer');
+        $curationActivity = CurationActivity::all()->first();
+        AssignVolunteerToAssignable::dispatch($volunteer, $curationActivity->expertPanels->first());
+        
+        Carbon::setTestNow(Carbon::now()->addDays(90));
+
+        $this->artisan('volunteers:notify-3m-followup');
+        
+        Notification::assertSentTo([$volunteer], ThreeMonthVolunteerFollowup::class);
+    }
+
+    /**
+     * @test
+     */
+    public function notification_renders_view()
+    {
+    }
+}
