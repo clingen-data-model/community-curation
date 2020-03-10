58c97eeee5422cfda8ab6e04372ec9e41323ed4a
TJ Ward <jward3@email.unc.edu>
2020-03-10 15:22:52 -0400

    Build UI to allow assignment of genes.

---
+++ resources/js/components/assignments/GeneGroupSelector.vue
@@ -0,0 +1,89 @@
+<template>
+    <div>
+        <ul class="list-unstyled mb-0">
+            <li v-for="(subAss, i) in assignment.subAssignments" :key="i"
+                :class="{'text-strike text-muted': (subAss.assignment_status_id == $store.state.configs.project.assignmentStatuses.retired)}"
+                class="d-flex justify-content-between"
+            >
+                <div>{{subAss.assignable.symbol}} <small class="text-muted">({{subAss.assignable.hgnc_id}})</small></div>
+                <div v-if="subAss.assignable.protocol_path" class="mr-3">
+                    <a :href="'/storage/'+subAss.assignable.protocol_path">Protocol</a>
+                </div>
+
+            </li>
+        </ul>
+        <button 
+            v-if="!addingGene && !assignment.needsAptitude" 
+            class="btn btn-sm btn-xs border" 
+            @click="addingGene = true"
+            :disabled="volunteer.volunteer_status_id == $store.state.configs.volunteers.statuses.retired"
+        >
+            Add Gene
+        </button>
+
+        <div v-if="addingGene" class="form-inline">
+            <select 
+                v-model="newGene" 
+                class="form-control form-control-sm"
+                :disabled="loadingGenes"
+            >
+                <option :value="null">Select&hellip;</option>
+                <option v-for="(gene, idx) in filteredGenes" :key="idx" :value="gene">
+                    {{gene.symbol}} - {{gene.hgnc_id}}
+                </option>
+            </select>
+            &nbsp;
+            <button class="btn btn-sm btn-primary" @click="emitSave">Save</button>
+            <button class="btn btn-sm btn-default" @click="cancelAddingGene">Cancel</button>
+        </div>
+    </div>
+</template>
+<script>
+import getAllGenes from '../../resources/genes/get_all_genes'
+
+export default {
+    props: {
+        assignment: {
+            required: true
+        },
+        volunteer: {
+            required: true
+        }
+    },
+    data() {
+        return {
+            loadingGenes: false,
+            newGene: null,
+            addingGene: false,
+            genes: []
+        }
+    },
+    computed: {
+        filteredGenes: function () {
+            return this.genes.filter(gene => {
+                return !this.assignment.subAssignments.map(subAss => subAss.assignable.id).includes(gene.id)
+            });
+        }
+    },
+    methods: {
+        emitSave() {
+            this.$emit('save', this.newGene)
+            this.addingGene = false;
+            this.newGene = null
+        },
+        cancelAddingGene() {
+            this.newGene = null
+            this.addingGene = false;
+            this.$emit('cancel');
+        },
+        async getGenes() {
+            this.genes = await getAllGenes();    
+            this.loadingGenes = true;
+            this.loadingGenes = false;
+        }
+    },
+    mounted() {
+        this.getGenes();
+    }
+}
+</script>
