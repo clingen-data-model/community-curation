5a4fea82c832ac551ccf489bf288db0592acb569
TJ Ward <jward3@email.unc.edu>
2020-03-10 15:22:52 -0400

    Build UI to allow assignment of genes.

---
+++ resources/js/components/assignments/TrainingAndAttestationControl.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div v-if="assignment.training.completed_at == null">
+            <div class="form-inline">
+                <div v-if="$store.state.user.notVolunteer()">
+                    <div v-show="setTrainingDate" class="form-inline">
+                        <label>Date completed:</label>
+                        &nbsp;
+                        <date-field v-model="newTrainingCompletedDate" class="form-control form-control-sm"></date-field>
+                        <button class="btn btn-sm btn-primary" @click="emitTrainingCompleted(assignment.training)">Save</button>
+                    </div>
+                    <button class="btn btn-sm btn-primary" v-show="!setTrainingDate" @click="setTrainingDate = true">Mark Training complete</button>
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            <div v-if="assignment.attestation.signed_at == null">
+                <a 
+                    :href="`/attestations/${assignment.attestation.id}/edit`" 
+                    class="btn btn-sm btn-primary"
+                >Sign attestation</a>
+            </div>
+            <!-- awaiting attestation -->
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        assignment: {
+            required: true
+        },
+    },
+    data() {
+        return {
+            newTrainingCompletedDate: null,
+            setTrainingDate: false            
+        }
+    },
+    computed: {
+
+    },
+    methods: {
+        emitTrainingCompleted(training) {
+            this.$emit('trainingcompleted', {
+                id: training.id,
+                completed_at: this.newTrainingCompletedDate
+            })
+        }
+    }
+}
+</script>
