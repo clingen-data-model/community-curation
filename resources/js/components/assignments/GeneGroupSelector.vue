4f7b88992c222f66f4a2c739bf09c8a7083013c9
TJ Ward <jward3@email.unc.edu>
2020-03-24 15:06:28 -0400

    Add status and status updating to AssignmentForm

--- resources/js/components/assignments/GeneGroupSelector.vue
+++ resources/js/components/assignments/GeneGroupSelector.vue
@@ -1,47 +1,83 @@
 <template>
     <div>
-        <ul class="list-unstyled mb-0">
+        <table class="table table-sm table-borderless mb-1">
+            <tr>
+                <th class="border-bottom" style="width: 50%"><small>Gene Symbol</small></th>
+                <th class="border-bottom"><small>Status</small></th>
+                <th class="border-bottom"><small>Protocol Link</small></th>
+            </tr>
+            <tr v-for="(subAss, i) in assignment.subAssignments" :key="i">
+                <td>
+                    {{subAss.assignable.symbol}}
+                    <small class="text-muted">({{subAss.assignable.hgnc_id}})</small>
+                </td>
+                <td>
+                    <status-badge :assignment="subAss"
+                        @assignmentsupdated="$emit('assignmentsupdated')"
+                    ></status-badge>
+                </td>
+                <td>
+                    <a :href="'/storage/'+subAss.assignable.protocol_path">Protocol</a>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <button 
+                        v-if="!addingGene && !assignment.needsAptitude" 
+                        class="btn btn-sm btn-xs border" 
+                        @click="addingGene = true"
+                        :disabled="volunteer.volunteer_status_id == $store.state.configs.volunteers.statuses.retired"
+                    >
+                        Add Gene
+                    </button>
+
+                    <div v-if="addingGene" class="form-inline">
+                        <select 
+                            v-model="newGene" 
+                            class="form-control form-control-sm"
+                            :disabled="loadingGenes"
+                        >
+                            <option :value="null">Select&hellip;</option>
+                            <option v-for="(gene, idx) in filteredGenes" :key="idx" :value="gene">
+                                {{gene.symbol}} - {{gene.hgnc_id}}
+                            </option>
+                        </select>
+                        &nbsp;
+                        <button class="btn btn-sm btn-primary" @click="emitSave">Save</button>
+                        <button class="btn btn-sm btn-default" @click="cancelAddingGene">Cancel</button>
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <!-- <ul class="list-unstyled mb-0">
             <li v-for="(subAss, i) in assignment.subAssignments" :key="i"
                 :class="{'text-strike text-muted': (subAss.assignment_status_id == $store.state.configs.project.assignmentStatuses.retired)}"
                 class="d-flex justify-content-between"
             >
-                <div>{{subAss.assignable.symbol}} <small class="text-muted">({{subAss.assignable.hgnc_id}})</small></div>
+                <div>
+                    {{subAss.assignable.symbol}} 
+                    <small class="text-muted">
+                        ({{subAss.assignable.hgnc_id}})
+                    </small>
+                    <status-badge :assignment="subAss"
+                        @assignmentsupdated="$emit('assignmentsupdated')"
+                    ></status-badge>
+                </div>
                 <div v-if="subAss.assignable.protocol_path" class="mr-3">
                     <a :href="'/storage/'+subAss.assignable.protocol_path">Protocol</a>
                 </div>
-
             </li>
-        </ul>
-        <button 
-            v-if="!addingGene && !assignment.needsAptitude" 
-            class="btn btn-sm btn-xs border" 
-            @click="addingGene = true"
-            :disabled="volunteer.volunteer_status_id == $store.state.configs.volunteers.statuses.retired"
-        >
-            Add Gene
-        </button>
-
-        <div v-if="addingGene" class="form-inline">
-            <select 
-                v-model="newGene" 
-                class="form-control form-control-sm"
-                :disabled="loadingGenes"
-            >
-                <option :value="null">Select&hellip;</option>
-                <option v-for="(gene, idx) in filteredGenes" :key="idx" :value="gene">
-                    {{gene.symbol}} - {{gene.hgnc_id}}
-                </option>
-            </select>
-            &nbsp;
-            <button class="btn btn-sm btn-primary" @click="emitSave">Save</button>
-            <button class="btn btn-sm btn-default" @click="cancelAddingGene">Cancel</button>
-        </div>
+        </ul> -->
     </div>
 </template>
 <script>
 import getAllGenes from '../../resources/genes/get_all_genes'
+import StatusBadge from './StatusBadge'
 
 export default {
+    components: {
+        StatusBadge
+    },
     props: {
         assignment: {
             required: true
