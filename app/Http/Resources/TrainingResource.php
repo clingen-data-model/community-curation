d0cde03dd2a9eb0282f56de5d702cfa0e499fe35
TJ Ward <jward3@email.unc.edu>
2020-03-25 17:21:32 -0400

    Refactor structured Assignments

---
+++ app/Http/Resources/TrainingResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Http\Resources\DefaultResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TrainingResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $data = parent::toArray($request);
+        $data['aptitude'] = new DefaultResource($this->whenLoaded('aptitude'));
+        $data['assignment'] = new DefaultResource($this->whenLoaded('assignment'));
+        $data['user'] = new DefaultResource($this->whenLoaded('user'));
+        return $data;
+    }
+}
