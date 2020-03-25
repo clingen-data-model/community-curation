2f875be6336b88cd718427163555452e467de2a5
TJ Ward <jward3@email.unc.edu>
2020-03-25 17:22:52 -0400

    await response when requesting all curation_activities

--- resources/js/resources/curation_activities/get_all_curation_activities.js
+++ resources/js/resources/curation_activities/get_all_curation_activities.js
@@ -2,7 +2,7 @@ const getAllCurationActivities = async function() {
     let data = JSON.parse(localStorage.getItem('curation-activities'));
 
     if (!data) {
-        window.axios.get('/api/curation-activities').then(response => {
+        await window.axios.get('/api/curation-activities').then(response => {
             data = response.data.data
             localStorage.setItem('curation-activities', JSON.stringify(data))
         });
