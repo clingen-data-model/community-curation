bec539f8bd93c98387856fb5d82a2de9eda0ef96
TJ Ward <jward3@email.unc.edu>
2020-04-16 14:09:12 -0400

    Send notifications for 6-month followup at 182, 189, 203 days after; Limit to first expert panel assigned

--- tests/Feature/VolunteerFollowupNotificationsTest.php
+++ tests/Feature/VolunteerFollowupNotificationsTest.php
@@ -29,15 +29,28 @@ class VolunteerFollowupNotificationsTest extends TestCase
     public function setup():void
     {
         parent::setup();
-        $this->volunteer = factory(User::class)->states('volunteer', 'comprehensive')->create([]);
-        $this->otherVol = factory(User::class)->states('volunteer', 'comprehensive')->create([]);
+        //Create 90 day volunteer
+        $this->volunteer90 = factory(User::class)->states('volunteer', 'comprehensive')->create([]);
+        $this->otherVol90 = factory(User::class)->states('volunteer', 'comprehensive')->create([]);
         
         $curationActivity = CurationActivity::all()->first();
-        AssignVolunteerToAssignable::dispatch($this->volunteer, $curationActivity->expertPanels->first());
-        AssignVolunteerToAssignable::dispatch($this->otherVol, $curationActivity->expertPanels->first());
+        AssignVolunteerToAssignable::dispatch($this->volunteer90, $curationActivity->expertPanels->first());
+        AssignVolunteerToAssignable::dispatch($this->otherVol90, $curationActivity->expertPanels->first());
 
         $this->survey = class_survey()::findBySlug('volunteer-three-month1');
-        $this->rsp = $this->survey->getNewResponse($this->otherVol);
+        $this->rsp = $this->survey->getNewResponse($this->otherVol90);
+        $this->rsp->finalize();
+
+        // create 6 month volunteer
+        $this->volunteer6m = factory(User::class)->states('volunteer', 'comprehensive')->create([]);
+        $this->otherVol6m = factory(User::class)->states('volunteer', 'comprehensive')->create([]);
+        
+        $curationActivity = CurationActivity::all()->first();
+        AssignVolunteerToAssignable::dispatch($this->volunteer6m, $curationActivity->expertPanels->first());
+        AssignVolunteerToAssignable::dispatch($this->otherVol6m, $curationActivity->expertPanels->first());
+
+        $this->survey = class_survey()::findBySlug('volunteer-six-month1');
+        $this->rsp = $this->survey->getNewResponse($this->otherVol6m);
         $this->rsp->finalize();
     }
 
@@ -51,16 +64,35 @@ class VolunteerFollowupNotificationsTest extends TestCase
 
         $this->artisan('volunteers:notify-followup');
         
-        Notification::assertSentTo([$this->volunteer, $this->otherVol], InitialFollowupNotification::class);
+        Notification::assertSentTo([$this->volunteer90, $this->otherVol90], InitialFollowupNotification::class);
     }
 
     /**
      * @test
      */
-    public function initial_notification_renders_correct_view()
+    public function comprehensive_volunteers_emailed_182_days_after_ep_assignment()
     {
-        $mailable = (new InitialFollowupNotification)->toMail($this->volunteer);
-        $this->assertEquals('email.volunteers.followups.initial_notification', $mailable->view);
+        Notification::fake();
+        Carbon::setTestNow(Carbon::now()->addDays(182));
+
+        $this->artisan('volunteers:notify-followup');
+        
+        Notification::assertSentTo([$this->volunteer6m, $this->otherVol6m], InitialFollowupNotification::class);
+    }
+    
+
+    /**
+     * @test
+     */
+    public function initial_notification_renders_correct_view_with_correct_url()
+    {
+        $mailable90 = (new InitialFollowupNotification(url('/volunteer-three-month')))->toMail($this->volunteer90);
+        $this->assertEquals('email.volunteers.followups.initial_notification', $mailable90->view);
+        $this->assertContains(url('/volunteer-three-month'), $mailable90->render());
+
+        $mailable6m = (new InitialFollowupNotification(url('/volunteer-six-month')))->toMail($this->volunteer6m);
+        $this->assertEquals('email.volunteers.followups.initial_notification', $mailable6m->view);
+        $this->assertContains(url('/volunteer-six-month'), $mailable6m->render());
     }
 
     /**
@@ -73,18 +105,38 @@ class VolunteerFollowupNotificationsTest extends TestCase
 
         $this->artisan('volunteers:notify-followup');
         
-        Notification::assertSentTo([$this->volunteer], FollowupReminder1::class);
+        Notification::assertSentTo([$this->volunteer90], FollowupReminder1::class);
 
-        Notification::assertNotSentTo([$this->otherVol], FollowupReminder1::class);
+        Notification::assertNotSentTo([$this->otherVol90], FollowupReminder1::class);
     }
 
     /**
      * @test
      */
-    public function remind_1_renders_correct_view()
+    public function sends_remind_1_189_days_after_ep_assignment_if_not_yet_responded()
     {
-        $mailable = (new FollowupReminder1)->toMail($this->volunteer);
-        $this->assertEquals('email.volunteers.followups.reminder_1', $mailable->view);
+        Notification::fake();
+        Carbon::setTestNow(Carbon::now()->addDays(189));
+
+        $this->artisan('volunteers:notify-followup');
+        
+        Notification::assertSentTo([$this->volunteer6m], FollowupReminder1::class);
+
+        Notification::assertNotSentTo([$this->otherVol6m], FollowupReminder1::class);
+    }
+
+    /**
+     * @test
+     */
+    public function remind_1_renders_correct_view_with_correct_url()
+    {
+        $mailable90 = (new FollowupReminder1(url('/volunteer-three-month')))->toMail($this->volunteer90);
+        $this->assertEquals('email.volunteers.followups.reminder_1', $mailable90->view);
+        $this->assertContains(url('/volunteer-three-month'), $mailable90->render());
+
+        $mailable6m = (new FollowupReminder1(url('/volunteer-six-month')))->toMail($this->volunteer6m);
+        $this->assertEquals('email.volunteers.followups.reminder_1', $mailable6m->view);
+        $this->assertContains(url('/volunteer-six-month'), $mailable6m->render());
     }
 
     /**
@@ -97,17 +149,37 @@ class VolunteerFollowupNotificationsTest extends TestCase
 
         $this->artisan('volunteers:notify-followup');
         
-        Notification::assertSentTo([$this->volunteer], FollowupReminder2::class);
+        Notification::assertSentTo([$this->volunteer90], FollowupReminder2::class);
 
-        Notification::assertNotSentTo([$this->otherVol], FollowupReminder2::class);
+        Notification::assertNotSentTo([$this->otherVol90], FollowupReminder2::class);
     }
 
     /**
      * @test
      */
-    public function remind_2_renders_correct_view()
+    public function sends_remind_2_203_days_after_ep_assignment_if_not_yet_responded()
     {
-        $mailable = (new FollowupReminder2)->toMail($this->volunteer);
-        $this->assertEquals('email.volunteers.followups.reminder_2', $mailable->view);
+        Notification::fake();
+        Carbon::setTestNow(Carbon::now()->addDays(203));
+
+        $this->artisan('volunteers:notify-followup');
+        
+        Notification::assertSentTo([$this->volunteer6m], FollowupReminder2::class);
+
+        Notification::assertNotSentTo([$this->otherVol6m], FollowupReminder2::class);
+    }
+
+    /**
+     * @test
+     */
+    public function remind_2_renders_correct_view_with_correct_url()
+    {
+        $mailable90 = (new FollowupReminder2(url('/volunteer-three-month')))->toMail($this->volunteer90);
+        $this->assertEquals('email.volunteers.followups.reminder_2', $mailable90->view);
+        $this->assertContains(url('/volunteer-three-month'), $mailable90->render());
+
+        $mailable6m = (new FollowupReminder2(url('/volunteer-six-month')))->toMail($this->volunteer6m);
+        $this->assertEquals('email.volunteers.followups.reminder_2', $mailable6m->view);
+        $this->assertContains(url('/volunteer-six-month'), $mailable6m->render());
     }
 }
