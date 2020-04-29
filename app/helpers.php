b2b0e8d52279fdee9217495a34407f6292d3a1fa
TJ Ward <jward3@email.unc.edu>
2020-04-29 12:40:04 -0400

    Seed upload categories

--- app/helpers.php
+++ app/helpers.php
@@ -1,9 +1,26 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
+
 if (!function_exists('renderQuery')) {
     function renderQuery($query)
     {
         $treated = preg_replace('/\?/', '"%s"', $query->toSql());
         return call_user_func_array('sprintf', array_merge([$treated], $query->getBindings()));
+    }
+}
+
+
+if (! function_exists('seedFromConfig')) {
+    function seedFromConfig($config, $modelClass)
+    {
+        Model::unguard();
+        $items = config($config);
+        foreach ($items as $slug => $id) {
+            $modelClass::updateOrCreate([
+              'id'=>$id,
+              'name'=>ucfirst(preg_replace('/-/', ' ', $slug))
+            ]);
+        }
     }
 }
