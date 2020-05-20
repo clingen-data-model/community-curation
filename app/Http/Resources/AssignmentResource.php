d824b0b2c566f65b216688af5f31977a90671b6b
TJ Ward <jward3@email.unc.edu>
2020-05-20 15:29:56 -0400

    Remove commented code; Fix test

--- app/Http/Resources/AssignmentResource.php
+++ app/Http/Resources/AssignmentResource.php
@@ -25,14 +25,5 @@ class AssignmentResource extends JsonResource
             'sub_assignments' => AssignmentResource::collection($this->whenLoaded('subAssignments')),
             'user_aptitudes' => new DefaultResource($this->whenLoaded('userAptitudes'))
         ];
-        // $data = parent::toArray($request);
-        // $data['assignable'] = new AssignableResource($this->whenLoaded('assignable'));
-        // $data['attestation'] = new AttestationResource($this->whenLoaded('attestation'));
-        // $data['status'] = new DefaultResource($this->whenLoaded('status'));
-        // $data['trainings'] = TrainingResource::collection($this->whenLoaded('trainings'));
-        // $data['userAptitudes'] = DefaultResource::collection($this->whenLoaded('trainings'));
-        // $data['volunteer'] = new DefaultResource($this->whenLoaded('volunteer'));
-
-        // return $data;
     }
 }
