dec76050cf4cedefc01f09912d133943a0e84d98
TJ Ward <jward3@email.unc.edu>
2020-06-10 13:27:26 -0400

    Add email composition UI

---
+++ resources/js/components/RichTextEditor.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <ckeditor :editor="editor" :value="value" @input="$emit('input', value)" :config="editorConfig"></ckeditor>
+    </div>
+</template>
+<script>
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import CKEditor from '@ckeditor/ckeditor5-vue';
+
+export default {
+    components: {
+        ckeditor: CKEditor.component
+    },
+    props: {
+        value: {
+            type: String,
+            default: ''
+        },
+        editorConfig: {
+            type: Object,
+            default: function () {
+                return {}
+            }
+        }
+    },
+    data() {
+        return {
+            editor: ClassicEditor
+        }
+    },
+}
+</script>
