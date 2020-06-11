ca7e2b85fa53a602382037fd5b3f2331b0bce3b4
TJ Ward <jward3@email.unc.edu>
2020-06-11 17:50:47 -0400

    Capture hypothes.is id in Baseline Attestation

--- tests/Feature/AttestationSignedTest.php
+++ tests/Feature/AttestationSignedTest.php
@@ -23,7 +23,7 @@ class AttestationSignedTest extends TestCase
     {
         parent::setUp();
         $this->volunteer = factory(User::class)->states('volunteer', 'comprehensive')->create();
-        AssignVolunteerToAssignable::dispatch($this->volunteer, CurationActivity::find(1));
+        AssignVolunteerToAssignable::dispatch($this->volunteer, CurationActivity::find(config('project.curation-activities.baseline')));
         $this->userAptitude = $this->volunteer->userAptitudes()->first();
         $this->userAptitude->update(['trained_at' => '2019-11-01']);
         $this->attestation = $this->volunteer->attestations()->unsigned()->first();
@@ -38,7 +38,7 @@ class AttestationSignedTest extends TestCase
 
         $this->assertDatabaseHas('assignments', [
             'assignable_type' => 'App\CurationActivity',
-            'assignable_id' => 1,
+            'assignable_id' => config('project.curation-activities.baseline'),
             'user_id' => $this->volunteer->id,
             'assignment_status_id' => config('project.assignment-statuses.trained')
         ]);
@@ -52,5 +52,18 @@ class AttestationSignedTest extends TestCase
         $this->attestation->update(['signed_at' => Carbon::now()]);
         
         $this->assertNotNull($this->userAptitude->fresh()->granted_at);
+    }
+
+    /**
+     * @test
+     */
+    public function hypothesis_id_added_to_user_when_attestation_signed()
+    {
+        $this->attestation->update(['signed_at' => Carbon::now(), 'data'=>['hypothesis_id' => 'porkypiney']]);
+
+        $this->assertDatabaseHas('users', [
+            'id' => $this->attestation->user_id,
+            'hypothesis_id' => 'porkypiney'
+        ]);
     }
 }
