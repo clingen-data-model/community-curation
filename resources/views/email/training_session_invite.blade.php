16940edef13dab0eff40aeb63f70c5b9bd1a786a
TJ Ward <jward3@email.unc.edu>
2020-06-22 15:12:22 -0400

    Sanitize user provided html by converting to markup and then converting back to HTML. Add tinymce to session edit form

--- resources/views/email/training_session_invite.blade.php
+++ resources/views/email/training_session_invite.blade.php
@@ -1,41 +1,30 @@
 <p>Hi {{$volunteer->first_name}},</p>
 
 <p>
-    You are invited to join our {{$trainingSession->topic->name}} training 
-    on {{$trainingSession->starts_at->addSeconds($timezone->getOffset($trainingSession->starts_at))->format('l, F j, Y \a\t g:i a')}} {{strtoupper($timezone->getAbbr())}}.  
+    This email is to inform you that a live training session for {{$trainingSession->topic->name}} will be held on 
+    {{$trainingSession->starts_at->addSeconds($timezone->getOffset($trainingSession->starts_at))->format('l, F j, Y \a\t g:i a')}} {{strtoupper($timezone->getAbbr())}}.
 </p>
+ 
+<p>Attendance for this live training session is required for volunteer training completion and curation assignment.</p>
+ 
+<p>Please see any optional notes for the session below.</p>
 
-<p>
-    Training will take place via an online at 
-    <a href="{{$trainingSession->url}}">{{$trainingSession->url}}</a>.
-</p>
+<blockquote>
+    {!! $trainingSession->invite_message !!}
+</blockquote>
 
-<p>
-    You need to complete this training to start volunteering with the Clinical Genome's community curation effort.
-</p>
-
-<p>
-    If you have any questions please reply to this email and we'll get your sorted out.
-</p>
-
-{!! $trainingSession->invite_message !!}
-
-<p>
-    We look forward to seeing you then!
-</p>
-
-<p>
-Cheers!
-</p>
+<p>If you have any questions or concerns, please contact us at <a href="mailto:volunteer@clinicalgeome.org">volunteer@clinicalgeome.org</a>.</p>
+ 
+<p>Thank you for your participation,</p>
+ 
+<p>The ClinGen Community Curation (C3) Working Group.</p>
 
 <br>
-<div>
-    Add the training session to your calendar:
-    <ul>
-        @foreach ($trainingSession->calendarLinks as $label => $url)
-            @if ($label != 'Apple & Outlook')
-                <li><a href="{{$url}}">{{$label}}</a></li>
-            @endif
-        @endforeach
-    </ul>
-</div>
+Add the training session to your calendar:
+<ul>
+    @foreach ($trainingSession->calendarLinks as $label => $url)
+        @if ($label != 'Apple & Outlook')
+            <li><a href="{{$url}}">{{$label}}</a></li>
+        @endif
+    @endforeach
+</ul>
