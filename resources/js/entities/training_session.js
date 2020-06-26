6393dedbf0e0875f8a8a7bf8ea488ab4305566c0
TJ Ward <jward3@email.unc.edu>
2020-06-26 10:35:03 -0400

    Add trainingSession js entity and use in components

--- resources/js/entities/training_session.js
+++ resources/js/entities/training_session.js
@@ -1,22 +1,86 @@
-const defaultTopicType = 'App\\CurationActivity';
-
 let TrainingSession = class {
-    constructor(data = {}) {
-
+    constructor(data = {})
+    {
         let defaults = {
-            topic_type: defaultTopicType,
+            id: null,
+            topic: {},
             topic_id: null,
-            url: null,
+            topic_type: 'App\\CurationActivity',
             starts_at: null,
             ends_at: null,
-            invite_message: null,
-            notes: null
+            url: null,
+            invite_message: '',
+            notes: '',
+            created_at: null,
+            updated_at: null,
         };
-
-        this.attributes = {...defaults, ...data};
-    }
-
-    setDuration(durationInMinutes) {
         
+        this.attributes = {...defaults, ...data};
+        this.dateFields = ['starts_at', 'ends_at', 'created_at', 'updated_at'];
+        this.appends = ['duration'];
+
+        for (const key in this.attributes) {
+            if (this.attributes.hasOwnProperty(key)) {
+                // this.attributes[key] = this.hydrateAttribute(key, data[key]);                
+                // Object.defineProperty(
+                //     this, 
+                //     key, 
+                //     {
+                //         get: function () {
+                //             return this.attributes[key]
+                //         },
+                //         set: function (value) {
+                //             this.attributes[key] = value
+                //         }
+                //     })
+
+                this[key] = this.hydrateAttribute(key, data[key]);
+        }
+        }
+
     }
+
+    get duration() {
+        return this.ends_at.diff(this.starts_at, 'minutes');
+    }
+
+    set duration(value) {
+        this.ends_at = this.starts_at.clone().add(value, 'minutes')
+    }
+
+    get started() {
+        return this.starts_at.isBefore(moment());
+    }
+    
+    get ended() {
+        return this.ends_at.isBefore(moment());
+    }
+
+    clone () {
+        return new TrainingSession(this.attributes);
+    }
+
+    getDuration() {
+        return this.duration;
+    }
+
+    exportAttributes () {
+        let appended = {};
+        return {...this.attributes,...{duration: this.duration}}
+    }
+
+    hydrateAttribute(key, value)
+    {
+        if (this.dateFields.includes(key)) {
+            let hydrated = window.moment();
+            if (value !== null) {
+                hydrated = window.moment(value);
+            }
+            return hydrated;
+        }
+        return value;
+    }
+
 }
+
+export default TrainingSession;
--- resources/js/resources/training_sessions/get_training_sessions.js
+++ resources/js/resources/training_sessions/get_training_sessions.js
@@ -1,14 +1,13 @@
 import queryStringFromParams from '../../http/query_string_from_params'
 import moment from 'moment'
+import TrainingSession from '../../entities/training_session'
 
 const getTrainingSessions = async function (params) {
     const url = '/api/training-sessions'+'?'+queryStringFromParams(params)
     
     let response = await window.axios(url);
     return response.data.data.map(item => {
-        item.starts_at = moment(item.starts_at)
-        item.ends_at = moment(item.ends_at)
-        return item;
+        return new TrainingSession(item);
     });
 }
 
