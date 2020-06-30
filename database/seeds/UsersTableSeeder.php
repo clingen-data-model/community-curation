a9615d91a02197e1e154b97944a2815fbe4c9d73
TJ Ward <jward3@email.unc.edu>
2020-06-30 10:27:11 -0400

    Update UsersTableSeeder to add admins

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -24,18 +24,49 @@ class UsersTableSeeder extends Seeder
         ]);
         $programmer->assignRole('programmer');
         
-        $admin = User::firstOrcreate(
-            [
-                'email' => 'courtney_thaxton@med.unc.edu',
-            ],
-            [
-                'first_name' => 'Courtney',
-                'last_name' => 'Thaxton',
-                'email' => 'courtney_thaxton@med.unc.edu',
-                'password' => Hash::make(uniqid()),
-                'country_id' => 225
-            ]
-        );
-        $admin->assignRole('admin');
+        // $admin = User::firstOrcreate(
+        //     [
+        //         'email' => 'courtney_thaxton@med.unc.edu',
+        //     ],
+        //     [
+        //         'first_name' => 'Courtney',
+        //         'last_name' => 'Thaxton',
+        //         'email' => 'courtney_thaxton@med.unc.edu',
+        //         'password' => Hash::make(uniqid()),
+        //         'country_id' => 225
+        //     ]
+        // );
+
+        $admins = [
+            ['first_name' => 'Courtney', 'last_name' => 'Thaxton', 'email' => 'courtney_thaxton@med.unc.edu'],
+            ['first_name' => 'Liz', 'last_name' => 'Kearns', 'email' => 'liz_kearns@med.unc.edu'],
+            [ 'first_name' => "Erin", 'last_name' =>	"Riggs", 'email' =>	"eriggs@geisinger.edu"],
+            [ 'first_name' => "Marina", 'last_name' =>	"DiStefano", 'email' =>	"mdistefano1@bwh.harvard.edu"],
+            [ 'first_name' => "Laura", 'last_name' =>	"Milko", 'email' =>	"laura_milko@med.unc.edu"],
+            [ 'first_name' => "Danielle", 'last_name' =>	"Azzaitti", 'email' =>	"dazzarit@broadinstitute.org"],
+            [ 'first_name' => "Jennifer", 'last_name' =>	"Goldstein", 'email' =>	"goldjen@email.unc.edu"],
+            [ 'first_name' => "Shruti", 'last_name' =>	"Rao", 'email' =>	"sr879@georgetown.edu"],
+            [ 'first_name' => "Deb", 'last_name' =>	"Ritter", 'email' =>	"dritter@bcm.edu"],
+            [ 'first_name' => "Taylor", 'last_name' =>	"Bingaman", 'email' =>	"tibingaman@geisinger.edu"],
+            [ 'first_name' => "Chris", 'last_name' =>	"Catlin", 'email' =>	"Chris.L.Catlin@kpchr.org"],
+            [ 'first_name' => "Christine", 'last_name' =>	"Pak", 'email' =>	"Christine.Pak@kpchr.org"],
+            [ 'first_name' => "Elizabeth", 'last_name' => 	"Kearns", 'email' =>	"liz_kearns@med.unc.edu"],
+        ];
+        
+        foreach ($admins as $admin) {
+            User::firstOrCreate(
+                [
+                    'email' => $admin['email'];
+                ],
+                array_merge(
+                    $admin,
+                    [
+                        'password' => Hash::make('tester'),
+                        'country_id' => 225
+                    ]
+                )
+            );
+        }
+
     }
 }
