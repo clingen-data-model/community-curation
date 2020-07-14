8e10cf19c6d02aa66c3df3ebfc473d23b8d285d2
TJ Ward <jward3@email.unc.edu>
2020-07-14 12:01:29 -0400

    remove test stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,8 +37,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{-- {{ config('app.name', 'Laravel') }} --}}
-                    I AM DEMO!!
+                    {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
