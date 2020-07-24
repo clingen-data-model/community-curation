42885e846a3ca12428b693fde15d53fe184d233e
TJ Ward <jward3@email.unc.edu>
2020-07-24 11:52:32 -0400

    Add notes model and relate to CurationGroup and User

---
+++ app/Note.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Venturecraft\Revisionable\RevisionableTrait;
+
+class Note extends Model
+{
+    use SoftDeletes;
+    use RevisionableTrait;
+
+    public $revisionCreationsEnabled = true;
+
+    public $fillable = [
+        'notable_type',
+        'notable_id',
+        'content',
+        'created_by'
+    ];
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by_id');
+    }
+
+    public function notable()
+    {
+        return $this->morphTo();
+    }
+}
---
+++ database/migrations/2020_07_24_141823_create_notes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->morphs('notable');
+            $table->text('content');
+            $table->unsignedBigInteger('created_by_id');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('created_by_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
