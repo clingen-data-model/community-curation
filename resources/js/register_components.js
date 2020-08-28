8169c0b1131fd0406c15fa2021f4e1e6393c8ba5
TJ Ward <jward3@email.unc.edu>
2020-08-28 16:15:55 -0400

    Clean up component registration.
    Use less verbose syntax and group global and form components to reduce number of chunks.

--- resources/js/register_components.js
+++ resources/js/register_components.js
@@ -3,301 +3,90 @@ window.Vue = require('vue');
 /**
  * Global components
  */
-window.Vue.component('non-volunteer', resolve => {
-    import(/* webpackChunkName: "non-volunteer" */ './components/NonVolunteer')
-        .then(NonVolunteer => {
-            resolve(NonVolunteer.default);
-        });
-});
+window.Vue.component('non-volunteer', () => import(/* webpackChunkName: "global-components" */ './components/NonVolunteer'));
 
-window.Vue.component('only-volunteer', resolve => {
-    import(/* webpackChunkName: "only-volunteer" */ './components/OnlyVolunteer')
-        .then(comp => {
-            resolve(comp.default);
-        });
-});
-window.Vue.component('global-progress', resolve => {
-    import(/* webpackChunkName: "global-progress" */ './components/GlobalProgress')
-        .then(comp => {
-            resolve(comp.default)
-        })
-})
-window.Vue.component('delete-button', resolve => {
-    import(/* webpackChunkName: "delete-button" */ './components/DeleteButton')
-        .then(comp => {
-            resolve(comp.default)
-        })
-})
-window.Vue.component('hypothesis-link', resolve => {
-    import(/* webpackChunkName: "hypothesis-link" */ './components/HypothesisLink')
-        .then(comp => {
-            resolve(comp.default)
-        })
-})
-window.Vue.component('impersonate-control', resolve => {
-    import(/* webpackChunkName: "impersonate-control" */ './components/ImpersonateControl')
-        .then(comp => {
-            resolve(comp.default)
-        })
-})
-window.Vue.component('rich-text', resolve => {
-    import(/* webpackChunkName: "rich-text" */ './components/RichTextEditor')
-        .then(comp => {
-            resolve(comp.default)
-        })
-})
-window.Vue.component('alerts', resolve => {
-    import(/* webpackChunkName: "alerts" */ './components/Alerts')
-        .then(comp => {
-            resolve(comp.default)
-        })
-})
+window.Vue.component('only-volunteer', () => import(/* webpackChunkName: "global-components" */ './components/OnlyVolunteer'));
+window.Vue.component('global-progress', () => import(/* webpackChunkName: "global-components" */ './components/GlobalProgress'));
+window.Vue.component('delete-button', () => import(/* webpackChunkName: "global-components" */ './components/DeleteButton'));
+window.Vue.component('hypothesis-link', () => import(/* webpackChunkName: "global-components" */ './components/HypothesisLink'));
+window.Vue.component('impersonate-control', () => import(/* webpackChunkName: "global-components" */ './components/ImpersonateControl'));
+window.Vue.component('alerts', () => import(/* webpackChunkName: "global-components" */ './components/Alerts'));
+window.Vue.component('help-button', () => import(/* webpackChunkName: "global-components" */ './components/HelpButton'));
 
-
-window.Vue.component('help-button', resolve => {
-    import(/* webpackChunkName: "help-button" */ './components/HelpButton')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});
-
+window.Vue.component('rich-text', () => import(/* webpackChunkName: "rich-text" */ './components/RichTextEditor'));
 
 /**
  * Layout components
  */
- 
-window.Vue.component('row', resolve => {
-    import(/* webpackChunkName: "row" */ './components/layout/Row')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});
-
-window.Vue.component('column', resolve => {
-    import(/* webpackChunkName: "column" */ './components/layout/Col')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});
+window.Vue.component('row', () => import(/* webpackChunkName: "global-components" */ './components/layout/Row'));
+window.Vue.component('column', () => import(/* webpackChunkName: "global-components" */ './components/layout/Col'));
 
 /**
  * Curation activities
  */
-window.Vue.component('curation-activity-list', resolve => {
-    import(/* webpackChunkName: "curation-activity-list" */ './components/curation_activities/CurationActivityList')
-        .then(CurationActivityList => {
-            resolve(CurationActivityList.default);
-        });
-});
-
-window.Vue.component('curation-activity-detail', resolve => {
-    import(/* webpackChunkName: "curation-activity-detail" */ './components/curation_activities/CurationActivityDetail')
-        .then(CurationActivityDetail => {
-            resolve(CurationActivityDetail.default);
-        });
-});
+window.Vue.component('curation-activity-list', () => import(/* webpackChunkName: "curation-activity-list" */ './components/curation_activities/CurationActivityList'));
+window.Vue.component('curation-activity-detail', () => import(/* webpackChunkName: "curation-activity-detail" */ './components/curation_activities/CurationActivityDetail'));
 
 /**
  * Curation groups
  */
-window.Vue.component('curation-group-list', resolve => {
-    import(/* webpackChunkName: "curation-group-list" */ './components/curation_groups/CurationGroupList')
-        .then(comp => {
-            resolve(comp.default);
-        });
-});
-window.Vue.component('curation-group-detail', resolve => {
-    import(/* webpackChunkName: "curation-group-detail" */ './components/curation_groups/CurationGroupDetail')
-        .then(comp => {
-            resolve(comp.default);
-        });
-});
+window.Vue.component('curation-group-list', () => import(/* webpackChunkName: "curation-group-list" */ './components/curation_groups/CurationGroupList'));
+window.Vue.component('curation-group-detail', () => import(/* webpackChunkName: "curation-group-detail" */ './components/curation_groups/CurationGroupDetail'));
 
 /**
  * Form components
  */
-
-window.Vue.component('question-block', resolve => {
-    import(/* webpackChunkName: "question-block" */ './components/forms/QuestionBlock')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
-
-window.Vue.component('radio-input', resolve => {
-    import(/* webpackChunkName: "radio-input" */ './components/forms/inputs/RadioInput')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
-
-window.Vue.component('radio-group', resolve => {
-    import(/* webpackChunkName: "radio-group" */ './components/forms/inputs/RadioGroup')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
-
-window.Vue.component('validation-error', resolve => {
-    import(/* webpackChunkName: "validation-error" */ './components/ValidationError')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
+window.Vue.component('question-block', () => import(/* webpackChunkName: "form-components" */ './components/forms/QuestionBlock'));
+window.Vue.component('radio-input', () => import(/* webpackChunkName: "form-components" */ './components/forms/inputs/RadioInput'));
+window.Vue.component('radio-group', () => import(/* webpackChunkName: "form-components" */ './components/forms/inputs/RadioGroup'));
+window.Vue.component('validation-error', () => import(/* webpackChunkName: "form-components" */ './components/ValidationError'));
 
 
-window.Vue.component('date-field', resolve => {
-    import(/* webpackChunkName: "date-field', resolve" */ './components/DateField')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});
+window.Vue.component('date-field', () => import(/* webpackChunkName: "form-components'" */ './components/DateField'));
 
 
 /**
  * Volunteer components
  */
-window.Vue.component('volunteer-index', resolve => {
-    import(/* webpackChunkName: "volunteer-index" */ './components/volunteers/VolunteerIndex')
-        .then(comp => {
-            resolve(comp.default);
-        });
-});
-window.Vue.component('volunteer-detail', resolve => {
-    import(/* webpackChunkName: "volunteer-detail" */ './components/volunteers/VolunteerDetail')
-        .then(comp => {
-            resolve(comp.default);
-        });
-});
-window.Vue.component('assignment-form', resolve => {
-    import(/* webpackChunkName: "assignment-form" */ './components/assignments/AssignmentForm')
-        .then(comp => {
-            resolve(comp.default);
-        });
-});
+window.Vue.component('volunteer-index', () => import(/* webpackChunkName: "volunteer-index" */ './components/volunteers/VolunteerIndex'));
+window.Vue.component('volunteer-detail', () => import(/* webpackChunkName: "volunteer-detail" */ './components/volunteers/VolunteerDetail'));
+window.Vue.component('assignment-form', () => import(/* webpackChunkName: "assignment-form" */ './components/assignments/AssignmentForm'));
 
     /**
      * Attestation Forms
      */
-    window.Vue.component('attestation-form', resolve => {
-        import(/* webpackChunkName: "attestation-form" */ './components/attestations/AttestationForm')
-        .then( comp => {
-            resolve(comp.default)
-        })
-    })
-
-    window.Vue.component('actionability-basic-form', resolve => {
-        import(/* webpackChunkName: "actionability-basic-form" */ './components/attestations/forms/ActionabilityBasic')
-        .then( comp => {
-            resolve(comp.default)
-        })
-    })
-
-    window.Vue.component('dosage-basic-form', resolve => {
-        import(/* webpackChunkName: "dosage-basic-form" */ './components/attestations/forms/DosageBasic')
-        .then( comp => {
-            resolve(comp.default);
-        });
-    });
-    window.Vue.component('baseline-basic-form', resolve => {
-        import(/* webpackChunkName: "baseline-basic-form" */ './components/attestations/forms/BaselineBasic')
-        .then( comp => {
-            resolve(comp.default);
-        });
-    });
-    window.Vue.component('baseline-genetic-form', resolve => {
-        import(/* webpackChunkName: "baseline-genetic-form" */ './components/attestations/forms/BaselineGenetic')
-        .then( comp => {
-            resolve(comp.default);
-        });
-    });
-    window.Vue.component('gene-basic-form', resolve => {
-        import(/* webpackChunkName: "gene-basic-form" */ './components/attestations/forms/GeneBasic')
-        .then( comp => {
-            resolve(comp.default);
-        });
-    });
-    window.Vue.component('somatic-basic-form', resolve => {
-        import(/* webpackChunkName: "somatic-basic-form" */ './components/attestations/forms/SomaticBasic')
-        .then( comp => {
-            resolve(comp.default);
-        });
-    });
-    window.Vue.component('variant-basic-form', resolve => {
-        import(/* webpackChunkName: "variant-basic-form" */ './components/attestations/forms/VariantBasic')
-        .then( comp => {
-            resolve(comp.default);
-        });
-    });
-    
-    
+    window.Vue.component('attestation-form', () => import(/* webpackChunkName: "attestation-form" */ './components/attestations/AttestationForm'))
+    window.Vue.component('actionability-basic-form', () => import(/* webpackChunkName: "actionability-basic-form" */ './components/attestations/forms/ActionabilityBasic'))
+    window.Vue.component('dosage-basic-form', () => import(/* webpackChunkName: "dosage-basic-form" */ './components/attestations/forms/DosageBasic'));
+    window.Vue.component('baseline-basic-form', () => import(/* webpackChunkName: "baseline-basic-form" */ './components/attestations/forms/BaselineBasic'));
+    window.Vue.component('baseline-genetic-form', () => import(/* webpackChunkName: "baseline-genetic-form" */ './components/attestations/forms/BaselineGenetic'));
+    window.Vue.component('gene-basic-form', () => import(/* webpackChunkName: "gene-basic-form" */ './components/attestations/forms/GeneBasic'));
+    window.Vue.component('somatic-basic-form', () => import(/* webpackChunkName: "somatic-basic-form" */ './components/attestations/forms/SomaticBasic'));
+    window.Vue.component('variant-basic-form', () => import(/* webpackChunkName: "variant-basic-form" */ './components/attestations/forms/VariantBasic'));
+     
 /**
  * Training Session
  */
- 
-window.Vue.component('training-session-list', resolve => {
-    import(/* webpackChunkName: "training-session-list" */ './components/training_sessions/TrainingSessionList')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});
-
-window.Vue.component('training-session-detail', resolve => {
-    import(/* webpackChunkName: "training-session-detail" */ './components/training_sessions/TrainingSessionDetail')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});
-
+window.Vue.component('training-session-list', () => import(/* webpackChunkName: "training-session-list" */ './components/training_sessions/TrainingSessionList'));
+window.Vue.component('training-session-detail', () => import(/* webpackChunkName: "training-session-detail" */ './components/training_sessions/TrainingSessionDetail'));
 
 /**
  * Reports
- */
- 
-window.Vue.component('report-form', resolve => {
-    import(/* webpackChunkName: "report-form" */ './components/reports/ReportForm')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
+ */ 
+window.Vue.component('report-form', () => import(/* webpackChunkName: "report-form" */ './components/reports/ReportForm'));
 
 /**
  * Notes
  */
- 
-window.Vue.component('notes-list', resolve => {
-    import(/* webpackChunkName: "notes-list" */ './components/notes/NotesList')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
+window.Vue.component('notes-list', () => import(/* webpackChunkName: "notes-list" */ './components/notes/NotesList'));
 
 /**
  * Faqs
  */
- 
-window.Vue.component('faq-list', resolve => {
-    import(/* webpackChunkName: "faq-list" */ './components/faq/FaqList')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
+window.Vue.component('faq-list', () => import(/* webpackChunkName: "faq-list" */ './components/faq/FaqList'));
 
 /**
  * Admin
  */
- 
-window.Vue.component('logged-in-users-list', resolve => {
-    import(/* webpackChunkName: "logged-in-users-list" */ './components/admin/LoggedInUsersList')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
-
-
-window.Vue.component('activity-metrics', resolve => {
-    import(/* webpackChunkName: "activity-metrics" */ './components/admin/ActivityMetrics')
-        .then( comp => {
-            resolve(comp.default);
-        });
-});;
+window.Vue.component('logged-in-users-list', () => import(/* webpackChunkName: "logged-in-users-list" */ './components/admin/LoggedInUsersList'));
+window.Vue.component('activity-metrics', () => import(/* webpackChunkName: "activity-metrics" */ './components/admin/ActivityMetrics'));
