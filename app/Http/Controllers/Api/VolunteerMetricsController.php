b133487ddd0d0949ffeb6da68e3f4727e2ced62e
TJ Ward <jward3@email.unc.edu>
2020-09-11 11:46:19 -0400

    Fix global metrics filtering and improve loading feedback

--- app/Http/Controllers/Api/VolunteerMetricsController.php
+++ app/Http/Controllers/Api/VolunteerMetricsController.php
@@ -8,6 +8,7 @@ use App\Services\Search\VolunteerSearchService;
 use App\VolunteerStatus;
 use App\VolunteerType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 class VolunteerMetricsController extends Controller
 {
@@ -24,10 +25,6 @@ class VolunteerMetricsController extends Controller
     {
         $params = $request->all();
 
-        $surveyDef = class_survey()::findBySlug('application1')->document;
-        $volunteerStatuses = VolunteerStatus::all()->keyBy('id');
-        $volunteerTypes = VolunteerType::all()->keyBy('id');
-
         $query = $this->volunteerSearchQuery($params, 'volunteer_status_id')
                     ->union($this->volunteerSearchQuery($params, 'volunteer_type_id'))
                     ->union($this->applicationSearchQuery($params, 'highest_ed'))
@@ -40,29 +37,62 @@ class VolunteerMetricsController extends Controller
                         return $group->pluck('count', 'key');
                     });
 
-        $counts->put('education', $counts->get('highest_ed')->keyBy(function ($count, $key) use ($surveyDef) {
-            return empty($key) ? 'Unknown' : $surveyDef->questions['highest_ed']->getOptionByValue($key)->label;
-        }));
-
-        $counts->put('self_description', $counts->get('self_desc')->keyBy(function ($count, $key) use ($surveyDef) {
-            return empty($key) ? 'Unknown' : $surveyDef->questions['self_desc']->getOptionByValue($key)->label;
-        }));
-
-        $counts->put('race/ethnicity', $counts->get('race_ethnicity')->keyBy(function ($count, $key) {
-            return empty($key) ? 'unknown' : json_decode($key)[0];
-        }));
-
-        $counts->put('volunteer_type_id', $counts->get('volunteer_type_id')->keyBy(function ($count, $key) use ($volunteerTypes) {
-            return empty($key) ? 'Unknown' : $volunteerTypes->get($key)->name;
-        }));
-
-        $counts->put('volunteer_status_id', $counts->get('volunteer_status_id')->keyBy(function ($count, $key) use ($volunteerStatuses) {
-            return empty($key) ? 'Unknown' : $volunteerStatuses->get($key)->name;
-        }));
-
-        $counts = $counts->except(['highest_ed', 'self_desc', 'race_ethnicity']);
+        $counts = $this->formatData($counts);
 
         return response($counts->toJson(), 200, ['Content-Type' => 'text/json']);
+    }
+
+    private function formatData($counts): Collection
+    {
+        $surveyDef = class_survey()::findBySlug('application1')->document;
+        $volunteerStatuses = VolunteerStatus::all()->keyBy('id');
+        $volunteerTypes = VolunteerType::all()->keyBy('id');
+
+        $paramMap = [
+            [
+                'attr' => 'highest_ed',
+                'label' => 'education',
+                'keyBy' => function ($count, $key) use ($surveyDef) {
+                    return empty($key) ? 'Unknown' : $surveyDef->questions['highest_ed']->getOptionByValue($key)->label;
+                },
+            ],
+            [
+                'attr' => 'self_desc',
+                'label' => 'self_description',
+                'keyBy' => function ($count, $key) use ($surveyDef) {
+                    return empty($key) ? 'Unknown' : $surveyDef->questions['self_desc']->getOptionByValue($key)->label;
+                },
+            ],
+            [
+                'label' => 'race/ethnicity',
+                'attr' => 'race_ethnicity',
+                'keyBy' => function ($count, $key) {
+                    return empty($key) ? 'unknown' : json_decode($key)[0];
+                },
+            ],
+            [
+                'label' => 'volunteer_type_id',
+                'attr' => 'volunteer_type_id',
+                'keyBy' => function ($count, $key) use ($volunteerTypes) {
+                    return empty($key) ? 'Unknown' : $volunteerTypes->get($key)->name;
+                },
+            ],
+            [
+                'label' => 'volunteer_status_id',
+                'attr' => 'volunteer_status_id',
+                'keyBy' => function ($count, $key) use ($volunteerStatuses) {
+                    return empty($key) ? 'Unknown' : $volunteerStatuses->get($key)->name;
+                },
+            ],
+        ];
+
+        foreach ($paramMap as $map) {
+            if ($counts->get($map['attr'])) {
+                $counts->put($map['label'], $counts->get($map['attr'])->keyBy($map['keyBy']));
+            }
+        }
+
+        return $counts->except(['highest_ed', 'self_desc', 'race_ethnicity']);
     }
 
     private function volunteerSearchQuery($params, $column, $label = null)
