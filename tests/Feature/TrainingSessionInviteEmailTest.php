1d4453a2a6483bef85fd3e1ef091e859c09a561d
TJ Ward <jward3@email.unc.edu>
2020-09-16 10:37:24 -0400

    Don't send invites to training invitees for past sessions.
    Regarding isse 84:https://github.com/clingen-data-model/community-curation/issues/84
    Admins want to be able to back-enter training sessions and invitiees.  Volutneers invited to these past sessions should not be sent an invite email.
    
    Add a test and check whether training session is past or upcoming
    before sending an email on invite.

--- tests/Feature/TrainingSessionInviteEmailTest.php
+++ tests/Feature/TrainingSessionInviteEmailTest.php
@@ -3,14 +3,12 @@
 namespace Tests\Feature;
 
 use App\Jobs\InviteVolunteersToTrainingSession;
-use Carbon\Carbon;
-use Tests\TestCase;
-use App\TrainingSession;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Notification;
 use App\Notifications\TrainingSessionInviteEmail;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\TrainingSession;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
 
 /**
  * @group training-sessions
@@ -19,25 +17,37 @@ class TrainingSessionInviteEmailTest extends TestCase
 {
     use DatabaseTransactions;
 
-    public function setup():void
+    public function setup(): void
     {
         parent::setup();
         $this->vol1 = $this->createVolunteer();
         $this->vol2 = $this->createVolunteer();
         $this->trainingSession = factory(TrainingSession::class)->create([
-            'starts_at' => Carbon::now()->addWeek(2)
+            'starts_at' => Carbon::now()->addWeek(2),
         ]);
     }
 
     /**
      * @test
      */
-    public function volunteer_sent_notification_when_invited_to_training_session()
+    public function volunteer_sent_notification_when_invited_to_future_training_session()
     {
         Notification::fake();
         // $this->trainingSession->attendees()->syncWithoutDetaching([$this->vol1->id, $this->vol2->id]);
         InviteVolunteersToTrainingSession::dispatch($this->trainingSession, collect([$this->vol1, $this->vol2]));
 
         Notification::assertSentTo([$this->vol1, $this->vol2], TrainingSessionInviteEmail::class);
+    }
+
+    /**
+     * @test
+     */
+    public function volunteer_not_sent_notification_when_invited_to_past_training_session()
+    {
+        Notification::fake();
+        $this->trainingSession->update(['starts_at' => Carbon::now()->subMinute(1)]);
+        InviteVolunteersToTrainingSession::dispatch($this->trainingSession, collect([$this->vol1, $this->vol2]));
+
+        Notification::assertNothingSent();
     }
 }
