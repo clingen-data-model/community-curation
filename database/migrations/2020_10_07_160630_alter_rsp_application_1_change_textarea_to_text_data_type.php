d5b9ffdd253b3145656c36165fd3a6c92afd639d
TJ Ward <jward3@email.unc.edu>
2020-10-07 12:31:01 -0400

    Fix length limitation and validation on application survey's adv_cert question

---
+++ database/migrations/2020_10_07_160630_alter_rsp_application_1_change_textarea_to_text_data_type.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterRspApplication1ChangeTextareaToTextDataType extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rsp_application_1', function (Blueprint $table) {
+            $table->text('adv_cert')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rsp_application_1', function (Blueprint $table) {
+            $table->string('adv_cert')->nullable()->change();
+        });
+    }
+}
