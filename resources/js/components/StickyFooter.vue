fcfbce86d7990efa5129fd59f138f282c277fd08
TJ Ward <jward3@email.unc.edu>
2020-10-07 14:08:24 -0400

    Add VolunteerIndex help video

---
+++ resources/js/components/StickyFooter.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="fixed-bottom py-2 bg-white border-top"> 
+        <button 
+            class="btn btn-light border float-right mr-2"
+            :class="'btn-'+buttonSize"
+            @click="expanded = !expanded"
+        >
+            <b-icon :icon="icon"></b-icon
+        ></button>
+        <div class="container" v-if="expanded">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        
+    },
+    data() {
+        return {
+            expanded: true
+        }
+    },
+    computed: {
+        buttonSize() {
+            return this.expanded ? 'sm' : 'xxs'
+        },
+        icon() {
+            return this.expanded ? 'chevron-down' : 'chevron-up'
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
