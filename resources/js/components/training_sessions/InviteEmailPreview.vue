e47d8f32412bc8e71b687bca55d6780929e88a19
TJ Ward <jward3@email.unc.edu>
2020-10-14 12:55:19 -0400

    Update invite email preview and show on form

--- resources/js/components/training_sessions/InviteEmailPreview.vue
+++ resources/js/components/training_sessions/InviteEmailPreview.vue
@@ -2,10 +2,33 @@
     <div>
         <button class="btn btn-light btn-sm border" @click="showPreview">Preview invite email</button>
         <b-modal v-model="showPreviewModal" title="Invite Preview" hide-footer>
-            <div v-show="!previewMarkup">
-                Loading the preview...
+            <div>
+                    <p>Hi [VOLUNTEER NAME],</p>
+
+                    <p>
+                        This email is to inform you that a live training session for {{topic.name}} will be held on 
+                        {{startsAt}}.
+                    </p>
+                    
+                    <p>Attendance for this live training session is required for volunteer training completion and curation assignment.</p>
+                    
+                    <blockquote v-html="inviteMessage">
+                    </blockquote>
+
+                    <p>If you have any questions or concerns, please contact us at <a href="mailto:volunteer@clinicalgeome.org">volunteer@clinicalgeome.org</a>.</p>
+                    
+                    <p>Thank you for your participation,</p>
+                    
+                    <p>The ClinGen Community Curation (C3) Working Group.</p>
+
+                    <br>
+                    Add the training session to your calendar:
+                    <ul>
+                        <li v-for="link in ['Google', 'Web-based Outlook', 'Yahoo']" :key="link">
+                            <a href="#">{{link}}</a>
+                        </li>
+                    </ul>                
             </div>
-            <div v-show="previewMarkup" v-html="previewMarkup"></div>
         </b-modal>
     </div>
 </template>
@@ -24,14 +47,34 @@ export default {
         }
     },
     computed: {
-
+        topic() {
+            console.info('this.trainingSession.topic', this.trainingSession.topic);
+            if (!this.trainingSession.topic || !this.trainingSession.topic.name) {
+                return {'name': '[TOPIC NAME]'};
+            }
+            console.info('fuckme in the ass', this.trainingSession.topic.name)
+            return this.trainingSession.topic
+        },
+        startsAt() {
+            return moment(this.trainingSession.starts_at).isValid()
+                    ? moment(this.trainingSession.starts_at).format('dddd, MMMM MM YYYY [at] hh:mm a') 
+                    : '[START DATE at TIME]'
+        },
+        inviteMessage() {
+            return this.trainingSession.invite_message || '[YOUR INVITE MESSAGE]';
+        }
     },
     methods: {
         showPreview() {
             this.showPreviewModal = true;
-            window.axios.get('/api/training-sessions/'+this.trainingSession.id+'/invite-preview')
-                .then(response => this.previewMarkup = response.data);
-            //get preview
+            if (this.trainingSession.id) {
+                this.getPreview();
+            }
+        },
+        async getPreview()
+        {
+            this.previewMarkup = await window.axios.get('/api/training-sessions/'+this.trainingSession.id+'/invite-preview')
+                .then(response => response.data);
         }
     }
 }
