7422aa472931cffff566d50fa2d6406d043d92b0
TJ Ward <jward3@email.unc.edu>
2020-10-16 17:40:32 -0400

    Update backpack to 4.0

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,4 +1,4 @@
-@extends('backpack::layout')
+@extends(backpack_view('blank'))
 
 @section('before_styles')
     <script src="{{ mix('/js/app.js') }}" defer></script>
@@ -9,32 +9,20 @@
     </style>
 @endsection
 
-@section('header')
-    <section class="content-header">
-		<h1>
-			{{ trans('backpack::base.dashboard') }}
-		</h1>
-		<ol class="breadcrumb">
-			<li><a href="{{ backpack_url() }}">{{ config('backpack.base.project_name') }}</a></li>
-			<li class="active">{{ trans('backpack::base.dashboard') }}</li>
-		</ol>
-    </section>
-@endsection
-
-
 @section('content')
     <div id="app">
 	<div class="box">
 		<div class="box-header with-border">
-			<div class="box-title w-100">Demographic Metrics</div>
+			<div class="box-title w-100 mb-2">Demographic Metrics</div>
 			<div class="box-body">
 				<global-metrics ref="glabalmetrics"></global-metrics>
 			</div>
 		</div>
 	</div>
+	<hr>
 	<div class="box">
 		<div class="box-header with-border">
-			<div class="box-title w-100">
+			<div class="box-title w-100 mb-2">
 				<button class="btn btn-sm btn-light border float-right" @click="$refs.activitymetrics.getMetrics()">
 					<b-icon icon="arrow-clockwise"></b-icon>
 				</button>
@@ -45,10 +33,11 @@
 			</div>
 		</div>
 	</div>
+	<hr>
     <row>
 		<div class="col-md-12">
 			<div class="box">
-				<div class="box-header with-border">
+				<div class="box-header with-border mb-2">
 					<div class="box-title">Logged in users</div>
 				</div>
 				<div class="box-body">
