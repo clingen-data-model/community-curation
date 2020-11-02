2c67b8a35da836343256d9c09dd696133949baad
TJ Ward <jward3@email.unc.edu>
2020-11-02 13:54:36 -0500

    Add on-hold volunteer status

--- resources/js/resources/volunteers/get_all_volunteer_statuses.js
+++ resources/js/resources/volunteers/get_all_volunteer_statuses.js
@@ -1,14 +1,26 @@
-const getAllVolunteerStatuses = async function () {
-    let data = JSON.parse(localStorage.getItem('volunteer-statuses'));
+import store from '../../store/index'
 
-    if (!data) {
-        data = await window.axios.get('/api/volunteer-statuses').then(response => {
-            localStorage.setItem('volunteer-statuses', JSON.stringify(response.data.data))
-            return response.data.data
+const getAllVolunteerStatuses = async function() {
+    // let data = JSON.parse(localStorage.getItem('volunteer-statuses'));
+
+    // // if (!data) {
+    // window.axios.get('/api/volunteer-statuses').then(response => {
+    //     localStorage.setItem('volunteer-statuses', JSON.stringify(response.data.data))
+    //     return response.data.data
+    // });
+    // // }
+
+    // return data;
+    const statusArray = Object.keys(store.state.configs.volunteers.statuses)
+        .map(name => {
+            return {
+                'name': name,
+                id: store.state.configs.volunteers.statuses[name]
+            }
         });
-    }
 
-    return data;
+    console.log(statusArray);
+    return statusArray;
 }
 
 export default getAllVolunteerStatuses;
