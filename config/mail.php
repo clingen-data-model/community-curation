847084d864d02a13664b9f25713ba74caddf8407
TJ Ward <jward3@email.unc.edu>
2020-12-03 16:33:00 -0500

    Reformat date for custom training email

--- config/mail.php
+++ config/mail.php
@@ -83,7 +83,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'volunteer@clinicalgenome.org'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
