63b19d32dd9664b4fdd81c108853d8fb32d3fa1d
TJ Ward <jward3@email.unc.edu>
2020-12-11 16:11:05 -0500

    Add soft deletes to Application model

--- app/Application.php
+++ app/Application.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Application extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'rsp_application_1';
     protected $fillable = [];
 
