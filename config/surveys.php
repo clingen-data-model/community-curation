ca3de6469f8a80b219931cbb4919086fb7d4a2ee
TJ Ward <jward3@email.unc.edu>
2020-12-14 16:29:25 -0500

    Autoformating

--- config/surveys.php
+++ config/surveys.php
@@ -4,18 +4,18 @@ return [
     'surveysPath' => base_path('resources/surveys'),
     'rulesPath' => app_path('Surveys'),
     'rulesNamespace' => 'App\\Surveys\\',
-    'routeGroup'=>['middleware' => ['web']],
-    'cacheDocuments'=> (env('APP_DEBUG')) ? false : true,
-    'chromeTemplate'=>'layouts.app',
-    'editAfterFinalized'=>false,
+    'routeGroup' => ['middleware' => ['web']],
+    'cacheDocuments' => (env('APP_DEBUG')) ? false : true,
+    'chromeTemplate' => 'layouts.app',
+    'editAfterFinalized' => false,
     'customTemplatePath' => base_path('resources/views'),
     'refusedLabel' => 'Refused',
     'refusedValue' => -77,
-    'autosave'=>[
+    'autosave' => [
         'enabled' => false,
         'frequency' => 10000, // time in miliseconds
         'notify' => true,
-        'notify_time' => 2500
+        'notify_time' => 2500,
     ],
     'default_templates' => [
         'page' => 'containers.page.page',
@@ -28,7 +28,7 @@ return [
         'number' => 'questions.number.number',
         'numeric_scale' => 'questions.number.numeric_scale',
         'question' => 'questions.text.default_text',
-        'time' => 'questions.time.time'
+        'time' => 'questions.time.time',
     ],
     'validation_messages' => [
         'curation_activity_2.different' => 'You have already selected this curation activity',
@@ -41,7 +41,7 @@ return [
     'bindings' => [
         'models' => [
             'Survey' => App\Survey::class,
-            'Response' => App\SurveyResponse::class
-        ]
+            'Response' => App\SurveyResponse::class,
+        ],
     ],
 ];
