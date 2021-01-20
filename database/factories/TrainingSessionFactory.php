b4b6fb0a1c668c7e6164a42aa84b321da86348ed
TJ Ward <jward3@email.unc.edu>
2021-01-20 15:44:48 -0500

    Fix TrainingSessionFactory and update dependencies

--- database/factories/TrainingSessionFactory.php
+++ database/factories/TrainingSessionFactory.php
@@ -8,28 +8,31 @@ use App\CurationActivity;
 use Faker\Generator as Faker;
 
 $factory->define(TrainingSession::class, function (Faker $faker) {
-    $datetime = new Carbon($faker->dateTimeBetween('-1 year', '+1 year'));
+    $fakeDateTime = $faker->dateTimeBetween('-1 year', '+1 year');
+    $start = new Carbon($fakeDateTime);
+    $end = $start->addHours(1);
     return [
         'topic_type' => CurationActivity::class,
         'topic_id' => CurationActivity::select('id')->get()->random()->id,
         'url' => $faker->url,
-        'starts_at' => $datetime,
-        'ends_at' => $datetime->addHours(1),
+        'starts_at' => $start,
+        'ends_at' => $end,
         'notes' => $faker->paragraph(1),
         'invite_message' => $faker->paragraph(2)
     ];
 });
 
 $factory->state(TrainingSession::class, 'future', function ($faker) {
-    $datetime = new Carbon($faker->dateTimeBetween('now', '2021-01-01'));
+    $start = new Carbon($faker->dateTimeBetween('+1 day', '+1 year'));
+    $end = $start->clone()->addHour();
     return [
-        'starts_at' => $datetime,
-        'ends_at' => $datetime->addHour()
+        'starts_at' => $start,
+        'ends_at' => $end
     ];
 });
 
 $factory->state(TrainingSession::class, 'past', function ($faker) {
-    $datetime = new Carbon($faker->dateTimeBetween('-1 year', 'now'));
+    $datetime = new Carbon($faker->dateTimeBetween('-1 year', '-1 day'));
     return [
         'starts_at' => $datetime,
         'ends_at' => $datetime->addHour()
