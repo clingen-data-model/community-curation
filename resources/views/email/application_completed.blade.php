a204cb64978c556f0d041268bae5a4e9196980f8
TJ Ward <jward3@email.unc.edu>
2021-02-23 09:06:17 -0500

    Update Application completed email.

--- resources/views/email/application_completed.blade.php
+++ resources/views/email/application_completed.blade.php
@@ -1,14 +1,15 @@
 <p>Dear {{$name}},</p>
 
 <p>
-    Thank you for your interest in ClinGen and for participating in the ClinGen volunteer survey!
-    Training is held on a quarterly basis, or as necessary.
+    Thank you for your interest in ClinGen and for participating in the ClinGen volunteer survey! Training is held on a quarterly basis, or as necessary.
 </p>
 
-<p>A representative from our Community Curation Committee (C3) will be in contact with you regarding training soon.</p>
+<p>
+    A representative from our ClinGen Community Curation (C3) working group will be in contact with you regarding training soon.
+</p>
 
 <p>
-    In the interim, if you have any questions please feel free to contact us at 
+    In the interim, if you have any questions please feel free to contact us at
     <a href="mailto:volunteer@clinicalgenome.org">volunteer@clinicalgenome.org</a>.
 
     To get started on ClinGen Community Curation web platform please <a href="{{url('/password/reset')}}">set your password</a>
@@ -19,5 +20,5 @@
 <p>
     Sincerely,
     <br />
-    The Community Curation Committee (C3)
+    The ClinGen Community Curation (C3) working group
 </p>   
