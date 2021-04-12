52eef5e252dbdf7c0bda57e069f0b007bff08976
TJ Ward <jward3@email.unc.edu>
2021-04-12 14:23:03 -0400

    Admin can set campaign display order in admin panel.

--- app/Http/Controllers/Admin/CampaignCrudController.php
+++ app/Http/Controllers/Admin/CampaignCrudController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Campaign;
-use App\Http\Requests\CampaignRequest as StoreRequest;
-use App\Http\Requests\CampaignRequest as UpdateRequest;
-use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\CrudPanel;
+use Illuminate\Support\Facades\DB;
+use App\Http\Requests\CampaignRequest as StoreRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use App\Http\Requests\CampaignRequest as UpdateRequest;
+use Backpack\CRUD\app\Http\Controllers\Operations\ReorderOperation;
 
 /**
  * Class CampaignCrudController.
@@ -19,6 +21,7 @@ class CampaignCrudController extends CrudController
     use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
     // use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use ReorderOperation;
 
     public function setup()
     {
@@ -41,6 +44,10 @@ class CampaignCrudController extends CrudController
 
         $this->crud->addColumns([
             [
+                'name' => 'display_order',
+                'type' => 'integer'
+            ],
+            [
                 'name' => 'starts_at',
                 'type' => 'date',
                 'format' => 'YYYY-MM-DD',
@@ -52,11 +59,14 @@ class CampaignCrudController extends CrudController
             ],
         ]);
 
+        $this->crud->removeField('display_order', 'created_at', 'updated_at');
+
         $this->crud->setRequiredFields(StoreRequest::class, 'create');
         $this->crud->setRequiredFields(UpdateRequest::class, 'edit');
 
         $this->crud->removeButton('delete');
         $this->crud
+            ->orderBy('display_order')
             ->orderBy('active', 'desc')
             ->orderBy('name');
     }
@@ -69,5 +79,35 @@ class CampaignCrudController extends CrudController
     protected function setupUpdateOperation()
     {
         $this->crud->setValidation(UpdateRequest::class);
+    }
+
+    protected function setupReorderOperation()
+    {
+        $this->crud->set('reorder.label', 'name');
+        $this->crud->set('reorder.max_level', 1);
+    }
+
+    public function saveReorder()
+    {
+        $this->crud->hasAccessOrFail('reorder');
+
+        $allEntries = \Request::input('tree');
+        
+        DB::beginTransaction();
+        foreach ($allEntries as $idx => $entry) {
+            dump($entry['item_id']);
+            if (is_null($entry['item_id'])) {
+                continue;
+            }
+            Campaign::find($entry['item_id'])
+                ->update([
+                    'display_order' => $idx
+                ]);
+        }
+        DB::commit();
+
+        return 'success for '.count($allEntries).' items';
+
+
     }
 }
