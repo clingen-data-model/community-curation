fabe7632a413dc329719c9d7f697a48957dfe54f
TJ Ward <jward3@email.unc.edu>
2021-04-27 14:50:54 -0400

    Add ability to email selected trainable volunteers on training session detail

--- resources/js/components/training_sessions/AttendeesManager.vue
+++ resources/js/components/training_sessions/AttendeesManager.vue
@@ -6,21 +6,34 @@
 </style>
 <template>
     <div>
-        <section>
-            <header>
-                <button class="btn btn-light border btn-xs float-right" @click="showEmailForm = !showEmailForm">
-                    {{showEmailForm ? `Cancel email` : `Email attendees`}}
-                </button>                
-                <h4>
-                    Attendees <small>({{attendees.length}})</small> 
-                    <button class="btn btn-default material-icons" :class="{rotate: loadingAttendees}" @click="getAttendees">cached</button>
-                </h4>
+        <invited-attendees :training-session="trainingSession"></invited-attendees>
+        <section class="mt-5">
+            <header class="d-flex justify-content-between">
+                <h5>
+                    {{showInactiveTrainables ? 'All' : 'Active'}} volunteers who need {{trainingSession.topic.name}} training 
+                    <small>({{filteredTrainable.length}})</small>
+                    <button class="btn btn-default material-icons" :class="{rotate: loadingVolunteers}" @click="getTrainableVolunteers">cached</button>
+                </h5>
+                <div class="d-flex align-items-start">
+                    <div class=" mr-2 mb-2 pr-2 border-right">
+                        <b-form-input type="text" v-model="trainableFilter" class="form-control form-control-sm mr-1" placeholder="search first or last" debounce="250"></b-form-input>
+                        <button @click="showInactiveTrainables = !showInactiveTrainables" class="btn btn-xs btn-default">
+                            {{showInactiveTrainables ? 'Hide' : 'Show'}} inactive volunteers
+                        </button>
+                    </div>
+
+                    <button class="btn btn-sm btn-primary mr-2" @click="inviteSelected" :disabled="!canInvite">{{inviteButtonText}}</button>
+                    <button class="btn btn-sm btn-light border  mr-2" @click="getSelectedEmails" :disabled="!canInvite">Copy emails</button>
+                    <button class="btn btn-light border btn-sm" @click="showEmailForm = !showEmailForm" :disabled="!canInvite">
+                        {{showEmailForm ? `Cancel email` : `Email selected`}}
+                    </button>                
+                </div>
             </header>
 
             <transition name="slide-fade">
                 <attendee-email-form 
                     :trainingSession="trainingSession" 
-                    :attendees="attendees"
+                    :attendees="selectedVolunteers"
                     v-show="showEmailForm" 
                     class="mb-2"
                     @sending="showEmailForm = false"
@@ -28,56 +41,6 @@
                     @canceled="showEmailForm = false"
                 ></attendee-email-form>
             </transition>
-            <transition name="slide-fade">
-                <div v-show="!showEmailForm">
-                    <div v-if="hasAttendees" style="overflow-y: scroll; border-bottom: 1px solid #eee">
-                        <b-table :fields="attendeeFields" :items="attendees" 
-                            small 
-                            sticky-header="305px"
-                            no-border-collapse
-                            class="border-bottom"
-                        >
-                            <template v-slot:cell(first_name)="{item}">
-                                <a :href="`/volunteers/${item.id}`">{{item.first_name}}</a>
-                            </template>
-                            <template v-slot:cell(last_name)="{item}">
-                                <a :href="`/volunteers/${item.id}`">{{item.last_name}}</a>
-                            </template>
-                            <template v-slot:cell(id)="{item}">
-                                <button v-if="sessionStarted && !item.training_complete"
-                                    @click="markTrainingComplete(item)"
-                                    class="btn btn-default btn-xs border" 
-                                >
-                                    Mark Training Complete
-                                </button>
-                                <small class="text-muted" v-show="item.training_complete">Training Complete</small>
-                            </template>
-                        </b-table>
-                    </div>
-                    <div v-else class="alert alert-light border">
-                            <span v-if="loadingAttendees">Loading...</span>
-                            <span v-else>No volunteers have been invited to attend.</span>
-                    </div>
-                </div>                
-            </transition>
-
-        </section>
-        <section class="mt-5">
-            <header class="clearfix">
-                <button class="btn btn-sm btn-light border float-right ml-2" @click="getSelectedEmails" :disabled="!canInvite">Copy emails</button>
-                <button class="btn btn-sm btn-primary float-right" @click="inviteSelected" :disabled="!canInvite">{{inviteButtonText}}</button>
-                <div class="float-right mr-2 mb-2 pr-2 border-right">
-                    <b-form-input type="text" v-model="trainableFilter" class="form-control form-control-sm mr-1" placeholder="search first or last" debounce="250"></b-form-input>
-                    <button @click="showInactiveTrainables = !showInactiveTrainables" class="btn btn-xs btn-default">
-                        {{showInactiveTrainables ? 'Hide' : 'Show'}} inactive volunteers
-                    </button>
-                </div>
-                <h5>
-                    {{showInactiveTrainables ? 'All' : 'Active'}} volunteers who need {{trainingSession.topic.name}} training 
-                    <small>({{filteredTrainable.length}})</small>
-                    <button class="btn btn-default material-icons" :class="{rotate: loadingVolunteers}" @click="getTrainableVolunteers">cached</button>
-                </h5>
-            </header>
 
             <b-table :fields="inviteFields" :items="filteredTrainable" 
                 v-if="trainableVolunteers.length > 0" 
@@ -127,9 +90,12 @@
                 <span v-if="loadingVolunteers">Loading...</span>
                 <span v-else>There are no more volunteers who need this training at this time.</span>
             </div>
-            <footer class="mt-1">
-                <button class="btn btn-sm btn-light border float-right ml-2" @click="getSelectedEmails" :disabled="!canInvite">Copy emails</button>
-                <button class="btn btn-sm btn-primary float-right" @click="inviteSelected" :disabled="!canInvite">{{inviteButtonText}}</button>
+            <footer class="mt-1 text-right">
+                <button class="btn btn-sm btn-primary mr-2" @click="inviteSelected" :disabled="!canInvite">{{inviteButtonText}}</button>
+                <button class="btn btn-sm btn-light border  mr-2" @click="getSelectedEmails" :disabled="!canInvite">Copy emails</button>
+                <button class="btn btn-light border btn-sm" @click="showEmailForm = !showEmailForm" :disabled="!canInvite">
+                    {{showEmailForm ? `Cancel email` : `Email selected`}}
+                </button>                
             </footer>
         </section>
         <b-modal v-model="showUpdateStatus" title="Update Volunteer Statuses">
@@ -158,20 +124,20 @@
 </template>
 <script>
 import moment from 'moment'
+import InvitedAttendees from './InvitedAttendees'
 import inviteAttendees from '../../resources/training_sessions/attendees/invite'
-import markTrainingComplete from '../../resources/trainings/mark_training_complete'
-import getAttendees from '../../resources/training_sessions/attendees/get_attendees'
 import getTrainableVolunteers from '../../resources/training_sessions/attendees/get_trainable_volunteers'
-import AttendeeEmailForm from './AttendeeEmailForm'
 import {mapMutations} from 'vuex'
 import updateVolunteer from '../../resources/volunteers/update_volunteer'
 import StatusBadge from '../assignments/StatusBadge'
 import copyToClipboard from '../../browser/copyToClipboard'
+import AttendeeEmailForm from './AttendeeEmailForm'
 
 export default {
     components: {
         AttendeeEmailForm,
-        StatusBadge
+        InvitedAttendees,
+        StatusBadge,
     },
     props: {
         trainingSession: {
@@ -181,11 +147,20 @@ export default {
     },
     data() {
         return {
+            showEmailForm: false,
             statusChange: 0,
             currentVolunteer: {assignments: []},
             test: false,
-            attendees: [],
             selectedVolunteers: [],
+            trainableVolunteers: [],
+            loadingAttendees: false,
+            loadingVolunteers: false,
+            selectAll: false,
+            inviting: false,
+            currentDateTime: moment(),
+            trainableFilter: null,
+            showInactiveTrainables: false,
+            showUpdateStatus: false,
             fields: [
                 {
                     key: 'first_name',
@@ -216,30 +191,17 @@ export default {
                 },
                 
             ],
-            trainableVolunteers: [],
-            loadingAttendees: false,
-            loadingVolunteers: false,
-            selectAll: false,
-            showEmailForm: false,
-            inviting: false,
-            currentDateTime: moment(),
-            trainableFilter: null,
-            showInactiveTrainables: false,
-            showUpdateStatus: false
         }
     },
     watch: {
         trainingSession() {
-            this.getAttendees();
+            // this.getAttendees();
             this.getTrainableVolunteers();
         }
     },
     computed: {
         sessionStarted() {
             return this.trainingSession.starts_at.isBefore(this.currentDateTime)
-        },
-        hasAttendees() {
-            return this.attendees.length > 0;
         },
         inviteFields() {
             return this.fields.concat([{
@@ -248,12 +210,6 @@ export default {
                 },
             {
                 key: 'training_sessions_count',
-                label: ''
-            }])
-        },
-        attendeeFields() {
-            return this.fields.concat([{
-                key: 'id',
                 label: ''
             }])
         },
@@ -302,7 +258,7 @@ export default {
             }
         },
         inviteSelected() {
-            const originalAttendees = JSON.parse(JSON.stringify(this.attendees));
+            // const originalAttendees = JSON.parse(JSON.stringify(this.attendees));
             const originalTrainable = JSON.parse(JSON.stringify(this.trainableVolunteers));
             this.inviting = true;
 
@@ -316,23 +272,18 @@ export default {
                 })
                 .catch(error => {
                     alert('There was a problem inviting attendees: '+error.response.statusText+'.');
-                    this.attendees = originalAttendees;
+                    // this.attendees = originalAttendees;
                     this.trainableVolunteers = originalTrainable;
                     this.selectAll = false;
                 })
                 .then(() => this.inviting = false);
         },
         getSelectedEmails() {
-            const emailString = this.selectedVolunteers.map(i => i.email).join(',');
+            const emailString = this.selectedVolunteers.map(i => i.email).join(';');
             copyToClipboard(emailString)
                 .then(() => {
                     this.addInfo('Email addresses for '+ this.selectedVolunteers.length + ' volunteers copied to your clipboard');
                 })
-        },
-        async getAttendees() {
-            this.loadingAttendees = true;
-            this.attendees = await getAttendees(this.trainingSession);
-            this.loadingAttendees = false;
         },
         async getTrainableVolunteers() {
             this.loadingVolunteers = true;
@@ -357,13 +308,6 @@ export default {
                 return;
             }
             this.selectedVolunteers.push(item);
-        },
-        markTrainingComplete (item) {
-            this.$set(item, 'training_complete', true)
-            markTrainingComplete(item.assignments[0].user_aptitudes[0].id, moment())
-                .then(response => {
-                    this.addInfo('Training marked completed for '+item.first_name+' '+item.last_name)
-                });
         },
         async markUnresponsive (volunteer) {
             const msg = `You are about to mark ${volunteer.name} unresponsive to their ${this.trainingSession.topic.name} variant curation activity assignment.  If you continue and the volunteer does not have other assignments their volunteer-status will be set to unresponsive. Do you want to continue?`
