7db785da65ec1c1885eb027bf9f583fd89c25916
TJ Ward <jward3@email.unc.edu>
2021-08-10 08:56:26 -0400

    Use newer MAIL_MAILER env instead of old MAIL_DRIVER

--- config/mail.php
+++ config/mail.php
@@ -12,7 +12,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_DRIVER', 'smtp'),
+    'default' => env('MAIL_MAILER', 'smtp'),
 
     /*
     |--------------------------------------------------------------------------
