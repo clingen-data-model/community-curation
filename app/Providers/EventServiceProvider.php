c297bf61b3e954c84aca38329dfe2dad41e538e3
Shift <shift@laravelshift.com>
2023-06-22 23:42:18 +0000

    Shift core files

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -97,4 +97,15 @@ class EventServiceProvider extends ServiceProvider
         }
         $this->listen['Illuminate\Mail\Events\MessageSent'] = [\App\Listeners\LogSentMessage::class];
     }
+
+    /**
+     * Determine if events and listeners should be automatically discovered.
+     *
+     * @return bool
+     */
+    public function shouldDiscoverEvents()
+    {
+        return false;
+    }
 }
+
