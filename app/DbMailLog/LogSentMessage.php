0edf7df22a09c3c89cbd3377a1a7a9eec7d7791f
tmbattey-2021 <tmbattey@proton.me>
2023-07-04 12:59:32 -0400

    event

---
+++ app/DbMailLog/LogSentMessage.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Mail\Events\SentMessage;
+
+class LogSentMessage
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @return void
+     */
+    public function handle(SentMessage $event)
+    {
+        $messageInfo = [
+            'to' => $event->message->getTo(),
+            'from' => $event->message->getFrom(),
+            'subject' => $event->message->getSubject(),
+            'body' => $event->message->getBody(),
+        ];
+
+        \Log::channel('mail')->info('Email sent', $messageInfo);
+    }
+}
