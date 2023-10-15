ad97b1b3f32e71de07ad1119f1957ea3cb54c357
Shift <shift@laravelshift.com>
2023-10-15 21:28:49 +0000

    Convert string references to `::class`
    PHP 5.5.9 adds the new static `class` property which provides the fully qualified class name. This is preferred over using strings for class names since the `class` property references are checked by PHP.

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -14,23 +14,23 @@ Route::prefix(config('backpack.base.route_prefix', 'admin'))->middleware('web', 
     Route::get('/survey-definitions', [App\Http\Controllers\Admin\SurveyDefinitionController::class, 'index']);
     Route::get('/survey-definitions/{slug}', [App\Http\Controllers\Admin\SurveyDefinitionController::class, 'show']);
 
-    Route::crud('/user', 'App\Http\Controllers\Admin\UserCrudController');
-    Route::crud('/working-group', 'App\Http\Controllers\Admin\WorkingGroupCrudController');
-    Route::crud('/curation-group', 'App\Http\Controllers\Admin\CurationGroupCrudController');
-    Route::crud('/gene', 'App\Http\Controllers\Admin\GeneCrudController');
-    Route::crud('/volunteer-type', 'App\Http\Controllers\Admin\VolunteerTypeCrudController');
-    Route::crud('/volunteer-status', 'App\Http\Controllers\Admin\VolunteerStatusCrudController');
-    Route::crud('/volunteer', 'App\Http\Controllers\Admin\VolunteerCrudController');
-    Route::crud('/custom-survey', 'App\Http\Controllers\Admin\CustomSurveyCrudController');
-    Route::crud('/campaign', 'App\Http\Controllers\Admin\CampaignCrudController');
-    Route::crud('/goal', 'App\Http\Controllers\Admin\GoalCrudController');
-    Route::crud('/interest', 'App\Http\Controllers\Admin\InterestCrudController');
-    Route::crud('/motivation', 'App\Http\Controllers\Admin\MotivationCrudController');
-    Route::crud('/self-description', 'App\Http\Controllers\Admin\SelfDescriptionCrudController');
-    Route::crud('/upload-category', 'App\Http\Controllers\Admin\UploadCategoryCrudController');
-    Route::crud('/faq', 'App\Http\Controllers\Admin\FaqCrudController');
+    Route::crud('/user', \App\Http\Controllers\Admin\UserCrudController::class);
+    Route::crud('/working-group', \App\Http\Controllers\Admin\WorkingGroupCrudController::class);
+    Route::crud('/curation-group', \App\Http\Controllers\Admin\CurationGroupCrudController::class);
+    Route::crud('/gene', \App\Http\Controllers\Admin\GeneCrudController::class);
+    Route::crud('/volunteer-type', \App\Http\Controllers\Admin\VolunteerTypeCrudController::class);
+    Route::crud('/volunteer-status', \App\Http\Controllers\Admin\VolunteerStatusCrudController::class);
+    Route::crud('/volunteer', \App\Http\Controllers\Admin\VolunteerCrudController::class);
+    Route::crud('/custom-survey', \App\Http\Controllers\Admin\CustomSurveyCrudController::class);
+    Route::crud('/campaign', \App\Http\Controllers\Admin\CampaignCrudController::class);
+    Route::crud('/goal', \App\Http\Controllers\Admin\GoalCrudController::class);
+    Route::crud('/interest', \App\Http\Controllers\Admin\InterestCrudController::class);
+    Route::crud('/motivation', \App\Http\Controllers\Admin\MotivationCrudController::class);
+    Route::crud('/self-description', \App\Http\Controllers\Admin\SelfDescriptionCrudController::class);
+    Route::crud('/upload-category', \App\Http\Controllers\Admin\UploadCategoryCrudController::class);
+    Route::crud('/faq', \App\Http\Controllers\Admin\FaqCrudController::class);
 
-    Route::crud('email', 'App\Http\Controllers\Admin\EmailCrudController');
+    Route::crud('email', \App\Http\Controllers\Admin\EmailCrudController::class);
     // Route::crud('notification', 'NotificationCrudController');
 }); // this should be the absolute last line of this file
 
