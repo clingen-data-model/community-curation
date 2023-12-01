4942833f24e35c2b8b9c3e26add36e866538e9cf
Bradford Powell <bpow@drpowell.org>
2023-12-01 14:12:13 -0500

    option to generate certificates since a certain date

--- app/Actions/TrainingCertificateGenerateForExisting.php
+++ app/Actions/TrainingCertificateGenerateForExisting.php
@@ -7,14 +7,18 @@ use Illuminate\Support\Str;
 use Illuminate\Console\Command;
 use Lorisleiva\Actions\Concerns\AsCommand;
 use App\Actions\TrainingCertificateGenerate;
+use Carbon\Carbon;
+
+// FIXME: the options here don't make a lot of sense because of the order of filtering... limit and offset are for # of volunteers, who may have had multiple trainings
 
 ini_set('memory_limit', '512M');
 class TrainingCertificateGenerateForExisting
 {
     use AsCommand;
 
-    public $commandSignature = 'volunteers:training:generate-certificates-for-existing {--limit= : # of volunteers} {--offset= : Offset at which to begin } {--dry-run : Print certificate params; don\'t actually generate certificates.} {--dry-run-quiet : print count of effected volunteers; don\'t generate certs.}';
-    
+    public $commandSignature = 'volunteers:training:generate-certificates-for-existing {--since= : First date (ISO-formatted like "2023-12-01") from which to include trainings} {--limit= : # of volunteers} {--offset= : Offset at which to begin } {--dry-run : Print certificate params; don\'t actually generate certificates.} {--dry-run-quiet : print count of effected volunteers; don\'t generate certs.}';
+
+    private $since = null;
     private $limit = null;
     private $offset = null;
     private $dryRun = false;
@@ -23,7 +27,7 @@ class TrainingCertificateGenerateForExisting
     public function __construct(private TrainingCertificateGenerate $certGen)
     {
     }
-    
+
 
     public function handle(): void
     {
@@ -45,7 +49,7 @@ class TrainingCertificateGenerateForExisting
         $query->get()
             ->each(function ($volunteer) {
                 $volunteer->userAptitudes->each(function ($userApt) use ($volunteer) {
-                    if (!$userApt->trained_at) {
+                    if ($userApt->trained_at < $this->since) {
                         return;
                     }
                     if ($this->dryRun) {
@@ -56,14 +60,21 @@ class TrainingCertificateGenerateForExisting
                 });
             });
     }
-    
+
     public function asCommand(Command $command)
     {
         $this->setLimit($command->option('limit'))
             ->setOffset($command->option('offset'))
             ->setDryRun($command->option('dry-run'))
             ->setDryRunQuiet($command->option('dry-run-quiet'))
+            ->setSince($command->option('since'))
             ->handle();
+    }
+
+    private function setSince($since)
+    {
+        $this->since = new Carbon($since == null ? 0 : $since);
+        return $this;
     }
 
     private function setLimit($limit)
@@ -75,24 +86,23 @@ class TrainingCertificateGenerateForExisting
     private function setOffset($offset)
     {
         $this->offset = ($offset > 0) ? $offset : null;
-
         return $this;
     }
-    
+
 
     private function setDryRun($dryRun)
     {
         $this->dryRun = $dryRun;
         return $this;
     }
-    
+
     private function setDryRunQuiet($dryRunQuiet)
     {
         $this->dryRunQuiet = $dryRunQuiet;
         return $this;
     }
-    
-    
+
+
 
     private function generateCert($volunteer, $userApt)
     {
@@ -102,7 +112,7 @@ class TrainingCertificateGenerateForExisting
             type: Str::kebab($userApt->aptitude->subject->name)
         );
     }
-    
+
     private function dumpCertParams($volunteer, $userApt): void
     {
         dump([
@@ -111,6 +121,6 @@ class TrainingCertificateGenerateForExisting
             'type' => Str::kebab($userApt->aptitude->subject->name)
         ]);
     }
-    
-    
+
+
 }
