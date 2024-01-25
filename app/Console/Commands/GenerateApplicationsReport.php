48798b6185eca9aa8b4263827aedef1245576ed1
Bradford Powell <bpow@drpowell.org>
2024-01-25 10:07:43 -0500

    comand version of applications reporting

---
+++ app/Console/Commands/GenerateApplicationsReport.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Services\Reports\ApplicationReportGenerator;
+use App\Services\Reports\ApplicationReportWriter;
+
+class GenerateApplicationsReport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'applications:report {--start_date=} {--end_date=} {--report=}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate report of all curator applications';
+
+    protected $generator;
+    protected $writer;
+
+    public function __construct(ApplicationReportGenerator $generator, ApplicationReportWriter $writer)
+    {
+        $this->generator = $generator;
+        $this->writer = $writer;
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->info('Generating report...');
+        $reportPath = $this->option('report') ?? 'application-report-'.now()->format('Y-m-d_His').'.xlsx';
+        $this->info($reportPath);
+        $this->info($this->option('start_date'));
+        $this->info($this->option('end_date'));
+
+        if ($this->option('start_date')) {
+            $this->generator
+                ->addConstraint(function ($q) {
+                    $q->where('finalized_at', '>=', $this->option('start_date'));
+                });
+        }
+
+        if ($this->option('end_date')) {
+            $this->generator
+                ->addConstraint(function ($q) {
+                    $q->where('finalized_at', '<=', $this->option('end_date'));
+                });
+        }
+
+        $data = $this->generator->generate();
+
+        $this->writer
+            ->setPath($reportPath)
+            ->writeData($data);
+    }
+}
