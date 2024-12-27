258f519c992cb994153140b508642523c1a846df
TJ Ward <yugen@ibiblio.org>
2024-12-27 11:57:18 -0500

    Make backfill action work as a command.

--- app/Actions/Reports/ApplicationReportRowBackfill.php
+++ app/Actions/Reports/ApplicationReportRowBackfill.php
@@ -2,22 +2,48 @@
 namespace App\Actions;
 
 use App\Application;
+use Lorisleiva\Actions\Concerns\AsCommand;
 use App\Actions\ApplicationReportRowCreate;
+use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Collection;
 
 class ApplicationReportRowBackfill
 {
+  use AsCommand;
+
+  public string $commandSignature = 'application-report-row:backfill';
+  private $rows;
+
   public function __construct(private ApplicationReportRowCreate $creatAction)
   {}
 
   public function handle()
   {
-    Application::query()
-      ->hasRespondent()
-      ->with('user', 'country', 'user.priorities', 'user.priorities.curationActivity', 'user.priorities.curationGroup')
-      ->finalized()
-      ->get()
-      ->each(function ($application) {
-      $this->creatAction->handle($application);
-    });
+      $this->getRows()->each(function ($application) {
+        $this->creatAction->handle($application);
+      });
+  }
+
+  public function asCommand(Command $command): void
+  {
+    $rowCount = $this->getRows()->count();
+    $start = microtime(true);
+    $command->info("Found $rowCount applications to backfill.");
+    $this->handle();
+    $end = microtime(true);
+    $command->info('Application report rows backfilled.');
+  }
+
+  private function getRows():Collection
+  {
+    if (!$this->rows) {
+      $this->rows = Application::query()
+        ->hasRespondent()
+        ->with('user', 'country', 'user.priorities', 'user.priorities.curationActivity', 'user.priorities.curationGroup')
+        ->finalized()
+        ->get();
+    }
+
+    return $this->rows;
   }
 }
