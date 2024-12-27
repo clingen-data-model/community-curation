c72667575f02b6a3c18c01fa9086330ba504dfa4
TJ Ward <yugen@ibiblio.org>
2024-12-27 12:26:00 -0500

    Extract Application Report building to acton.

---
+++ app/Actions/Reports/ApplicationReportBuild.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
+use App\Services\Reports\ApplicationReportWriter;
+use App\Services\Reports\ApplicationReportGenerator;
+
+final class ApplicationReportBuild
+{
+    public function __construct(
+        private ApplicationReportGenerator $generator,
+        private ApplicationReportWriter $writer
+    ) {
+    }
+
+    public function handle(array $params): string {
+      $filePath = storage_path('app/reports/application-report-'.Carbon::now()->format('Y-m-d_H:i:s').'.xlsx');
+
+      if (isset($params['start_date'])) {
+          $this->generator
+              ->addConstraint(function ($q) use ($params) {
+                  $q->where('finalized_at', '>=', $params['start_date']);
+              });
+      }
+
+      if (isset($params['end_date'])) {
+          $this->generator
+              ->addConstraint(function ($q) use ($params) {
+                  $q->where('finalized_at', '<=', $params['end_date']);
+              });
+      }
+
+      $data = $this->generator->generate($params);
+
+      if ($data->count() == 0) {
+          session()->flash('warning', 'Nothing matched your filters.');
+
+          return redirect()->back();
+      }
+
+      $this->writer
+          ->setPath($filePath)
+          ->writeData($data)
+          ->addMetadata($this->getMetadata($params))
+          ->closeWriter();
+
+      return $filePath;
+    }
+
+    private function getMetadata($filterParams): Collection
+    {
+        $metadata = collect();
+        if (count($filterParams) == 0) {
+            $metadata->push(collect(['filter' => '', 'value' => '']));
+
+            return $metadata;
+        }
+        foreach ($filterParams as $key => $value) {
+            if ($key == 'page') {
+                continue;
+            }
+
+            $label = $key;
+            $valueLabel = $value;
+
+            if (substr($key, -3) === '_id') {
+                $label = ucfirst(str_replace('_', ' ', (preg_replace('/_id$/', '', $key))));
+                $model = 'App\\'.ucfirst(Str::camel(preg_replace('/_id$/', '', $key)));
+                $valueLabel = 'unassigned';
+
+                if ($value != -1) {
+                    $instance = $model::find($value);
+                    $valueLabel = ($instance) ? $instance->name : '?';
+                }
+            }
+
+            $metadata->push(collect(['filter' => $label, 'value' => $valueLabel]));
+        }
+
+        return $metadata;
+    }
+}
--- app/Http/Controllers/ApplicationReportController.php
+++ app/Http/Controllers/ApplicationReportController.php
@@ -4,88 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Services\Reports\ApplicationReportGenerator;
 use App\Services\Reports\ApplicationReportWriter;
+use ApplicationReportBuild;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class ApplicationReportController extends Controller
 {
-    protected $generator;
-
-    protected $writer;
-
-    public function __construct(ApplicationReportGenerator $generator, ApplicationReportWriter $writer)
+    public function __construct(private ApplicationReportBuild $buildReport)
     {
-        $this->generator = $generator;
-        $this->writer = $writer;
     }
 
     public function index(Request $request)
     {
-        $filePath = storage_path('app/reports/application-report-'.Carbon::now()->format('Y-m-d_H:i:s').'.xlsx');
-
-        if ($request->start_date) {
-            $this->generator
-                ->addConstraint(function ($q) use ($request) {
-                    $q->where('finalized_at', '>=', $request->start_date);
-                });
-        }
-
-        if ($request->end_date) {
-            $this->generator
-                ->addConstraint(function ($q) use ($request) {
-                    $q->where('finalized_at', '<=', $request->end_date);
-                });
-        }
-
-        $filterParams = $request->all();
-        $data = $this->generator->generate($filterParams);
-
-        if ($data->count() == 0) {
-            session()->flash('warning', 'Nothing matched your filters.');
-
-            return redirect()->back();
-        }
-
-        $this->writer
-            ->setPath($filePath)
-            ->writeData($data);
-
         return response()
-            ->download($filePath)
+            ->download($this->buildReport->handle($request->all()))
             ->deleteFileAfterSend();
-    }
-
-    private function getMetadata($filterParams)
-    {
-        $metadata = collect();
-        if (count($filterParams) == 0) {
-            $metadata->push(collect(['filter' => '', 'value' => '']));
-
-            return $metadata;
-        }
-        foreach ($filterParams as $key => $value) {
-            if ($key == 'page') {
-                continue;
-            }
-
-            $label = $key;
-            $valueLabel = $value;
-
-            if (substr($key, -3) === '_id') {
-                $label = ucfirst(str_replace('_', ' ', (preg_replace('/_id$/', '', $key))));
-                $model = 'App\\'.ucfirst(Str::camel(preg_replace('/_id$/', '', $key)));
-                $valueLabel = 'unassigned';
-
-                if ($value != -1) {
-                    $instance = $model::find($value);
-                    $valueLabel = ($instance) ? $instance->name : '?';
-                }
-            }
-
-            $metadata->push(collect(['filter' => $label, 'value' => $valueLabel]));
-        }
-
-        return $metadata;
     }
 }
