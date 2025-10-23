904d2b31ff5aaa838c00e5470b41eaff8f29cea0
setiadha <setiadha@unc.edu>
2025-10-23 15:17:44 -0400

    Enhaced certificate generate. Fix CCDB-45

--- app/Actions/TrainingCertificateGenerate.php
+++ app/Actions/TrainingCertificateGenerate.php
@@ -17,106 +17,105 @@ use Illuminate\Support\Facades\View;
 use Lorisleiva\Actions\Concerns\AsJob;
 use Lorisleiva\Actions\Concerns\AsListener;
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
 class TrainingCertificateGenerate
 {
-    use AsJob;
-    use AsListener;
+    use AsJob, AsListener;
 
-    private Mpdf $converter;
+    public string $jobQueue = 'pdf';
+    public int $tries = 3;
+    public int $backoff = 10;
+    public bool $afterCommit = true;
 
     public function __construct(private DocumentCreate $documentCreate)
-    {
-        $this->converter = $this->setupMpdf();
+    {        
     }
 
     public function handle(User $user, string $type, Carbon $date): Upload
-    {
-        $activitiesByName = CurationActivity::all()->keyBy(function ($ca) {
-            return strtolower(Str::kebab($ca->name));
-        });
+    {        
+        $mpdf = $this->setupMpdf();
+        $activitiesByName = CurationActivity::all()->keyBy(fn ($ca) => strtolower(Str::kebab($ca->name)));
 
         if (!$activitiesByName->keys()->contains($type)) {
             throw new \InvalidArgumentException('valid types include '.$activitiesByName->keys()->join(', ', ', or '));
         }
 
         $data = [
-            'name' => $user->name, 
-            'type' => $type, 
+            'name' => $user->name,
+            'type' => $type,
             'curationActivity' => $activitiesByName->get($type)->legacy_name,
             'date' => $date
         ];
-        $view = View::make('certificate', $data);
+        $html = View::make('certificate', $data)->render();
+
+        Storage::makeDirectory('training_certificates');
+        Storage::makeDirectory('mpdf_temp');
 
         $relativePath = 'training_certificates/'.Uuid::uuid4()->toString().'.pdf';
         $storagePath = storage_path('app/'.$relativePath);
-        $this->converter->WriteHTML($view->render());
-        $this->converter->output($storagePath, \Mpdf\Output\Destination::FILE);
-        
+
+        try {
+            $mpdf->WriteHTML($html);
+            $mpdf->Output($storagePath, \Mpdf\Output\Destination::FILE);
+        } catch (\Throwable $e) {
+            Log::error('Training certificate PDF generation failed', [
+                'user_id' => $user->id,
+                'type' => $type,
+                'date' => (string)$date,
+                'message' => $e->getMessage(),
+            ]);
+            throw $e;
+        }
+
         return $this->documentCreate
-            ->handle(
-                filepath: $relativePath,
-                originalFileName: $type.'-training-certificate.pdf',
-                user_id: $user->id,
-                upload_category_id: config('project.upload-categories.training-certificate')
-            );  
+        ->handle(
+            filepath: $relativePath,
+            originalFileName: $type.'-training-certificate.pdf',
+            user_id: $user->id,
+            upload_category_id: config('project.upload-categories.training-certificate')
+        );
     }
-    
+
     public function asListener(TrainingCompleted $event): void
     {
         static::dispatch(
-            user: $event->userAptitude->user, 
+            user: $event->userAptitude->user,
             type: Str::kebab($event->userAptitude->aptitude->subject->name),
             date: $event->userAptitude->trained_at,
-        );
-    
+        )->onQueue($this->jobQueue);
     }
-    
+
     private function setupMpdf(): Mpdf
     {
         $defaultConfig = (new ConfigVariables())->getDefaults();
-        $fontDirs = array_merge(array_merge($defaultConfig['fontDir'], [
+        $fontDirs = array_merge($defaultConfig['fontDir'], [
             base_path('fonts/Lora'),
             base_path('fonts/Montserrat'),
             base_path('fonts/OpenSans'),
             base_path('fonts/Pacifico'),
-        ]));
-        
+        ]);
+
         $defaultFontConfig = (new FontVariables())->getDefaults();
         $fontdata = $defaultFontConfig['fontdata'] + [
-            'lora' => [
-                'R' => '/Lora-Regular.ttf',
-                'I' => '/Lora-Italic.ttf'
-            ],
-            'montserrat' => [
-                'R' => '/Montserrat-Regular.ttf',
-                'B' => '/Montserrat-ExtraBold.ttf'
-            ],
-            'opensans' => [
-                'R' => '/OpenSans-Medium.ttf',
-                'I' => '/OpenSans-MediumItalic.ttf',
-                'B' => '/OpenSans-Bold.ttf'
-            ],
-            'pacifico' => [
-                'R' => '/Pacifico-Regular.ttf',
-            ]
+            'lora' => ['R' => '/Lora-Regular.ttf', 'I' => '/Lora-Italic.ttf'],
+            'montserrat' => ['R' => '/Montserrat-Regular.ttf', 'B' => '/Montserrat-ExtraBold.ttf'],
+            'opensans' => ['R' => '/OpenSans-Medium.ttf', 'I' => '/OpenSans-MediumItalic.ttf', 'B' => '/OpenSans-Bold.ttf'],
+            'pacifico' => ['R' => '/Pacifico-Regular.ttf']
         ];
-        
+
         $mpdf = new Mpdf([
             'showImagesErrors' => true,
             'orientation' => 'L',
             'fontDir' => $fontDirs,
             'fontdata' => $fontdata,
-            'tempDir' => storage_path('/mpdf_temp'),
-            'useKerning' => true
+            'tempDir' => storage_path('app/mpdf_temp'),
+            'useKerning' => true,
         ]);
         $mpdf->showImageErrors = false;
-        $mpdf->setBasePath(url('/'));
-
-        if (!file_exists(storage_path('/mpdf_temp'))) {
-            mkdir(storage_path('/mpdf_temp'));
-        }
+        $mpdf->setBasePath(public_path());
 
         return $mpdf;
     }
-    
 }
