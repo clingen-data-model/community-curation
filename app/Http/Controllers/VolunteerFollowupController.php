c4587088987e65212ab2ff823c98edc60345fe96
Bradford Powell <bpow@drpowell.org>
2026-02-01 22:23:44 -0500

    Update VolunteerFollowupController to use SurveyJS
    Replace server-rendered SurveyControlService with the new Blade
    template that mounts the SurveyForm Vue component. The store()
    method and POST route are removed since submissions now go
    through the API controller.

--- app/Http/Controllers/VolunteerFollowupController.php
+++ app/Http/Controllers/VolunteerFollowupController.php
@@ -2,82 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\URL;
-use Sirs\Surveys\SurveyControlService;
-use Illuminate\Support\Carbon;
 
 class VolunteerFollowupController
 {
     public function show(Request $request, $surveySlug, $responseId = null)
     {
-        $this->setPreviousLocation($request);
-
-        $respondent = Auth::user();
-        $response = $this->getResponse($respondent, $surveySlug, $responseId);
-        if ($response && is_string($response->finalized_at)) {
-            $response->finalized_at = Carbon::parse($response->finalized_at);
-        }
-
-        if ($respondent->id == $response->respondent_id
-            || Auth::user()->hasAnyRole(['programmer', 'super-admin', 'admin'])) {
-            $control = new SurveyControlService($request, $response);
-
-            return $control->showPage();
-        }
-
-        throw new AuthorizationException('You do not have permission to view this survey response');
-    }
-
-    public function store(Request $request, $surveySlug, $id = null)
-    {
-        $respondent = Auth::user();
-        $response = $this->getResponse($respondent, $surveySlug, $id);
-
-        if (
-            $respondent->id == $response->respondent_id
-            || Auth::user()->hasAnyRole(['programmer', 'super-admin', 'admin'])
-        ) {
-            if ($response->finalized_at) {
-                throw new AuthorizationException('You can not update a finalized followup survey response');
-            }
-
-            $control = new SurveyControlService($request, $response);
-
-            return $control->saveAndContinue();
-        }
-
-        throw new AuthorizationException('You do not have permission to update this survey response');
+        return view('surveys.take', [
+            'slug' => $surveySlug,
+            'redirectUrl' => '/',
+        ]);
     }
 
     public function sixMonth()
     {
-        return redirect('volunteer-followup/volunteer-six-month1');
+        return redirect('volunteer-followup/volunteer-six-month.1');
     }
 
     public function threeMonth()
     {
-        return redirect('volunteer-followup/volunteer-three-month1');
+        return redirect('volunteer-followup/volunteer-three-month.1');
     }
 
-    protected function getResponse($respondent, $surveySlug, $responseId)
-    {
-        $survey = class_survey()::where('slug', $surveySlug)->firstOrFail();
-        if ($responseId) {
-            return $survey->responses()->find($responseId);
-        }
-
-        return $survey->getLatestResponse($respondent);
-    }
-
-    protected function setPreviousLocation(Request $request)
-    {
-        $previous = $request->session()->pull('survey_previous');
-        if (!preg_match('/\/survey\//', URL::previous())) {
-            $previous = URL::previous();
-        }
-        $request->session()->put('survey_previous', $previous);
-    }
 }
